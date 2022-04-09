0631e87431af92be2b8cbd9cb5f694fd85c24154
Dan Loman <daniel.h.loman@gmail.com>
2022-04-09 11:43:59 -0700

    Add StytchClient

---
+++ Sources/StytchCore/StytchClient/StytchClient.swift
@@ -0,0 +1,41 @@
+import Networking
+import Foundation
+
+public final class StytchClient {
+    static let instance: StytchClient = .init()
+
+    let networkingClient: NetworkingClient
+
+    var configuration: Configuration?
+
+    let jsonDecoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        // TODO: confirm decoding/encoding strategies
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+        return decoder
+    }()
+
+    let jsonEncoder: JSONEncoder = {
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        encoder.dateEncodingStrategy = .iso8601
+        return encoder
+    }()
+
+    private init(
+        networkingClient: NetworkingClient = .init()
+    ) {
+        self.networkingClient = networkingClient
+    }
+
+    public static func configure(environment: Configuration.Environment = .production, publicToken: String) {
+        instance.configuration = .init(environment: environment, publicToken: publicToken)
+        instance.networkingClient.headerProvider = { [weak instance] in
+            guard let configuration = instance?.configuration else { return [:] }
+            return [
+                "Authorization": "Bearer \(configuration.publicToken)"
+            ]
+        }
+    }
+}
