94c14b45d63feac78dcf2cbe84a0844a40313041
Dan Loman <daniel.h.loman@gmail.com>
2022-04-09 11:48:36 -0700

    Add StytchClient networking helpers

---
+++ Sources/StytchCore/StytchClient/StytchClient+Networking.swift
@@ -0,0 +1,72 @@
+import Foundation
+import Networking
+
+extension StytchClient {
+    func post<Parameters: Encodable, Response: Decodable>(
+        parameters: Parameters,
+        path: String,
+        completion: @escaping ((Result<Response, Error>) -> Void)
+    ) {
+        guard let configuration = configuration else {
+            completion(.failure(fatalError("BLAH")))
+            return
+        }
+        do {
+            let data = try StytchClient.instance.jsonEncoder.encode(parameters)
+            StytchClient.instance.performRequest(
+                .post(data),
+                url: configuration.baseURL.appendingPathComponent(path),
+                completion: completion
+            )
+        } catch {
+            completion(.failure(error))
+        }
+    }
+
+    func get<Response: Decodable>(
+        queryItems: [URLQueryItem],
+        path: String,
+        completion: @escaping ((Result<Response, Error>) -> Void)
+    ) {
+        guard let configuration = configuration else {
+            completion(.failure(fatalError("BLAH")))
+            return
+        }
+        guard var urlComponents = URLComponents(url: configuration.baseURL.appendingPathComponent(path), resolvingAgainstBaseURL:  false) else {
+            completion(.failure(fatalError("BLAH")))
+            return
+        }
+        var queryItems = urlComponents.queryItems ?? []
+        queryItems.append(contentsOf: queryItems)
+        urlComponents.queryItems = queryItems
+        guard let url = urlComponents.url else {
+            completion(.failure(fatalError("BLAH")))
+            return
+        }
+
+        performRequest(.get, url: url, completion: completion)
+    }
+
+    private func performRequest<Response: Decodable>(
+        _ method: NetworkingClient.Method = .get,
+        url: URL,
+        completion: @escaping ((Result<Response, Error>) -> Void)
+    ) {
+
+        networkingClient.performRequest(
+            method,
+            url: url
+        ) { [unowned self] result in
+            // TODO: verify network response code code
+            completion(
+                result.flatMap { data, _ in
+                    do {
+                        return .success(try self.jsonDecoder.decode(Response.self, from: data))
+                    } catch {
+                        return .failure(error)
+                    }
+                }
+            )
+        }
+    }
+}
