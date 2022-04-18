ea9a4671bc71f584d7a31c8b836d7d3d5579d002
Dan Loman <daniel.h.loman@gmail.com>
2022-04-17 19:07:59 -0700

    Add very basic DemoApp

--- StytchDemo/Shared/ContentView.swift
+++ StytchDemo/Shared/ContentView.swift
@@ -1,14 +1,23 @@
+import StytchCore
 import SwiftUI
 
 struct ContentView: View {
+    let hostUrl: URL
+    var session: Session?
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if let session = session {
+            SessionView(session: session)
+        } else {
+            LoginView(hostUrl: hostUrl)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        // swiftlint:disable:next force_unwrapping
+        ContentView(hostUrl: URL(string: "https://stytch.com")!, session: nil)
     }
 }
+
