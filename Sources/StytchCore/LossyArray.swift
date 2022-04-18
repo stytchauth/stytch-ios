cf0f8ed1e42040e848753e5275c0d628774e5887
Dan Loman <daniel.h.loman@gmail.com>
2022-04-17 18:44:43 -0700

    Add helper types LossyArray/Path, and URL extension

---
+++ Sources/StytchCore/LossyArray.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+@propertyWrapper
+struct LossyArray<T: Decodable>: Decodable {
+    private struct AnyDecodableValue: Decodable {}
+
+    var wrappedValue: [T]
+
+    init(wrappedValue: [T]) {
+        self.wrappedValue = wrappedValue
+    }
+
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+
+        var elements: [T] = []
+
+        while !container.isAtEnd {
+            do {
+                elements.append(try container.decode(T.self))
+            } catch {
+                print(error)
+                _ = try? container.decode(AnyDecodableValue.self)
+            }
+        }
+
+        self.wrappedValue = elements
+    }
+}
