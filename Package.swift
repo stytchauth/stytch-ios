c4c43496b00d61f58702844292ab6e938fbd9cb7
Dan Loman <daniel.h.loman@gmail.com>
2022-04-17 18:32:49 -0700

    Clean up/flesh out Package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,17 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Stytch",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS("11.3"), .macOS(.v10_13), .tvOS(.v11), .watchOS(.v4)],
     products: [
         .library(name: "StytchCore", targets: ["StytchCore"]),
     ],
     dependencies: [],
     targets: [
         .target(name: "StytchCore", dependencies: ["Networking"]),
-        .target(name: "Networking", dependencies: []),
-        .testTarget(name: "StytchTests", dependencies: ["StytchCore"]),
+        .target(name: "Networking"),
+        .testTarget(name: "StytchCoreTests", dependencies: ["StytchCore"]),
     ]
 )
