e717bcec138c34a25e4e7454da357e9c76b3a0f1
Dan Loman <daniel.h.loman@gmail.com>
2022-04-19 20:52:47 -0700

    Add intial deeplink handler

--- Sources/StytchCore/StytchClient/StytchClient.swift
+++ Sources/StytchCore/StytchClient/StytchClient.swift
@@ -24,4 +24,37 @@ public struct StytchClient {
             ]
         }
     }
+
+    // sourcery: AsyncVariants
+    public static func handle(url: URL, completion: @escaping Completion<DeeplinkHandledStatus>) {
+        guard
+            let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+            let queryItems = components.queryItems,
+            let typeQuery = queryItems.first(where: { $0.name == "type" }),
+            let tokenQuery = queryItems.first(where: { $0.name == "token" }), let token = tokenQuery.value
+        else {
+            completion(.success(.right(url)))
+            return
+        }
+
+        switch typeQuery.value {
+        case "em":
+            // TODO: pass session duration as function parameter
+            magicLinks.authenticate(parameters: .init(token: token, sessionDuration: .init(rawValue: 30))) { result in
+                completion(result.map { .left(($0, url)) })
+            }
+        default:
+            completion(.failure(StytchError(message: "Unrecognized deeplink type")))
+        }
+    }
+}
+
+public extension StytchClient {
+    typealias DeeplinkHandledStatus = Either<(AuthenticateResponse, URL), URL>
+}
+
+public enum Either<Left, Right> {
+    case left(Left)
+    case right(Right)
+}
 }
