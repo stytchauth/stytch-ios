b98e1cc94e8c60a96ce799bc6ba1d4abe85aa1d8
Dan Loman <daniel.h.loman@gmail.com>
2022-04-19 20:54:19 -0700

    Add codegen for url handling

---
+++ Sources/StytchCore/Generated/StytchClient+AsyncVariants.generated.swift
@@ -0,0 +1,41 @@
+// Generated using Sourcery 1.8.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+import Foundation
+
+// MARK: - handle Combine
+#if canImport(Combine)
+import Combine
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
+public extension StytchClient {
+    static func handle(url: URL) -> AnyPublisher<DeeplinkHandledStatus, Error> {
+        return Deferred { 
+            Future({ promise in
+                handle(url: url, completion: promise)
+            })
+        }
+        .eraseToAnyPublisher()
+    }
+}
+#endif
+
+// MARK: - handle Async/Await
+#if compiler(>=5.5) && canImport(_Concurrency)
+public extension StytchClient {
+    #if compiler(>=5.5.2)
+    @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
+    static func handle(url: URL) async throws -> DeeplinkHandledStatus {
+        try await withCheckedThrowingContinuation { continuation in
+            handle(url: url, completion: continuation.resume)
+        }
+    }
+    #else
+    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+    static func handle(url: URL) async throws -> DeeplinkHandledStatus {
+        try await withCheckedThrowingContinuation { continuation in
+            handle(url: url, completion: continuation.resume)
+        }
+    }
+    #endif
+}
+#endif
