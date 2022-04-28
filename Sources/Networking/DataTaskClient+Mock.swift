168a43c6e225b70b50fd30da31c991d2b6dee8d8
Dan Loman <daniel.h.loman@gmail.com>
2022-04-28 11:32:31 -0700

    Lint

--- Sources/Networking/DataTaskClient+Mock.swift
+++ Sources/Networking/DataTaskClient+Mock.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 #if DEBUG
-public extension DataTaskClient {
-    // swiftlint:disable force_unwrapping
-    static func mock(returning result: Result<Data, Error>) -> DataTaskClient {
-        .init { request, _, completion in
-            completion(
-                result.map { ($0, .init(url: request.url!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: request.allHTTPHeaderFields)!) }
-            )
-            return .init(dataTask: nil)
+    public extension DataTaskClient {
+        // swiftlint:disable force_unwrapping
+        static func mock(returning result: Result<Data, Error>) -> DataTaskClient {
+            .init { request, _, completion in
+                completion(
+                    result.map { ($0, .init(url: request.url!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: request.allHTTPHeaderFields)!) }
+                )
+                return .init(dataTask: nil)
+            }
         }
+        // swiftlint:enable force_unwrapping
     }
-    // swiftlint:enable force_unwrapping
-}
 #endif
