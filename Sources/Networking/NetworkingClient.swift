10ffddb990bcedda15e45f472a6887c18745b4df
Dan Loman <daniel.h.loman@gmail.com>
2022-04-28 11:41:15 -0700

    Remove unused code

--- Sources/Networking/NetworkingClient.swift
+++ Sources/Networking/NetworkingClient.swift
@@ -71,48 +71,12 @@ public extension NetworkingClient {
         case nonHttpResponse
     }
 
+    // A handle wrapping the underlying DataTask which can easily be extended to pass through progress or cancellation functionality
     struct TaskHandle {
         weak var dataTask: URLSessionDataTask?
-
-        public var progress: Progress { dataTask?.progress ?? Progress(totalUnitCount: 0) }
 
         public init(dataTask: URLSessionDataTask?) {
             self.dataTask = dataTask
         }
-
-        public func cancel() {
-            dataTask?.cancel()
-        }
     }
 }
-
-#if DEBUG
-    public extension URLRequest {
-        var curlString: String {
-            guard let url = url else { return "" }
-            var baseCommand = #"curl "\#(url.absoluteString)""#
-
-            if httpMethod == "HEAD" {
-                baseCommand += " --head"
-            }
-
-            var command = [baseCommand]
-
-            if let method = httpMethod, method != "GET", method != "HEAD" {
-                command.append("-X \(method)")
-            }
-
-            if let headers = allHTTPHeaderFields {
-                for (key, value) in headers where key != "Cookie" {
-                    command.append("-H '\(key): \(value)'")
-                }
-            }
-
-            if let data = httpBody, let body = String(data: data, encoding: .utf8) {
-                command.append("-d '\(body)'")
-            }
-
-            return command.joined(separator: " \\\n\t")
-        }
-    }
-#endif
