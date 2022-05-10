0b185f107c18ac989ee1b7a04c832f6c75059614
Dan Loman <daniel.h.loman@gmail.com>
2022-05-09 19:44:45 -0700

    Try to authenticate existing session for Demo app

--- StytchDemo/Shared/StytchDemoApp.swift
+++ StytchDemo/Shared/StytchDemoApp.swift
@@ -15,6 +15,15 @@ struct StytchDemoApp: App {
                         publicToken: "public-token-test-9e306f84-4f6a-4c23-bbae-abd27bcb90ba", // TODO: extract this token
                         hostUrl: hostUrl
                     )
+                    do {
+                        let response = try await StytchClient.sessions.authenticate(parameters: .init(duration: 30))
+                        switch response {
+                        case let .authenticated(response):
+                            session = response.session
+                        case .unauthenticated:
+                            break
+                        }
+                    } catch {}
                 }
                 .onOpenURL { url in
                     Task {
