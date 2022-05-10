936acf750bf9610dd9028d0df00bd8aae2573f42
Dan Loman <daniel.h.loman@gmail.com>
2022-05-09 17:37:12 -0700

    Add debugging from SessionView

--- StytchDemo/Shared/SessionView.swift
+++ StytchDemo/Shared/SessionView.swift
@@ -18,6 +18,26 @@ struct SessionView: View {
             Text("Started at: " + session.startedAt.formatted(date: .abbreviated, time: .shortened))
             Text("Expires at: " + session.expiresAt.formatted(date: .abbreviated, time: .shortened))
             Text("User agent: " + session.attributes.userAgent)
+            Button("Fetch index", action: {
+                var request: URLRequest = .init(url: URL(string: "https://dan-stytch.github.io")!)
+                StytchClient.sessions.sessionToken.map { request.addValue($0, forHTTPHeaderField: "X-Stytch-Token") }
+
+                let task = URLSession.shared.dataTask(
+                    with: request,
+                    completionHandler: { data, blah, error in
+                        print(data.flatMap { String(data: $0, encoding: .utf8) } ?? "no data")
+                    }
+                )
+
+                print("Request: ", request)
+                print("Original request headers: ", task.originalRequest?.allHTTPHeaderFields ?? [])
+                print("Cookies for url: ", HTTPCookieStorage.shared.cookies(for: request.url!) ?? [])
+                Task {
+                    print("Cookies for task: ", await HTTPCookieStorage.shared.cookies(for: task) ?? [])
+                }
+
+                task.resume()
+            })
         }
     }
 }
