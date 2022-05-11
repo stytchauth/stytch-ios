0ff87dd22cee4e89bbe5c11bdfb34c2b2422effe
Dan Loman <daniel.h.loman@gmail.com>
2022-05-11 12:38:31 -0700

    Fix typo

--- Sources/StytchCore/EmptyCodable.swift
+++ Sources/StytchCore/EmptyCodable.swift
@@ -1,2 +1,2 @@
-/// An empty type to allow decoding/decoding the absence of a value within various generic Decodable types.
+/// An empty type to allow encoding/decoding the absence of a value within various generic Decodable types.
 public struct EmptyCodable: Codable {}
