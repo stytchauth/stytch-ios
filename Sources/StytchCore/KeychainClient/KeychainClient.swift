0544142c9d0b028aa2d7d40f9e9425d3217d1e4b
Dan Loman <daniel.h.loman@gmail.com>
2022-05-11 13:16:01 -0700

    Remove comments, reorganize

--- Sources/StytchCore/KeychainClient/KeychainClient.swift
+++ Sources/StytchCore/KeychainClient/KeychainClient.swift
@@ -1,5 +1,42 @@
 import Foundation
-import Security
+
+struct KeychainClient {
+    private let getItem: (Self, Item) throws -> String?
+
+    private let setValueForItem: (Self, String, Item) throws -> Void
+
+    private let removeItem: (Self, Item) throws -> Void
+
+    private let resultExists: (Item) -> Bool
+
+    init(
+        getItem: @escaping (KeychainClient, KeychainClient.Item) throws -> String?,
+        setValueForItem: @escaping (KeychainClient, String, KeychainClient.Item) throws -> Void,
+        removeItem: @escaping (KeychainClient, KeychainClient.Item) throws -> Void,
+        resultExists: @escaping (KeychainClient.Item) -> Bool
+    ) {
+        self.getItem = getItem
+        self.setValueForItem = setValueForItem
+        self.removeItem = removeItem
+        self.resultExists = resultExists
+    }
+
+    func get(_ item: Item) throws -> String? {
+        try getItem(self, item)
+    }
+
+    func set(_ value: String, for item: Item) throws {
+        try setValueForItem(self, value, item)
+    }
+
+    func remove(_ item: Item) throws {
+        try removeItem(self, item)
+    }
+
+    func resultExists(for item: Item) -> Bool {
+        resultExists(item)
+    }
+}
 
 extension KeychainClient {
     struct Item {
@@ -51,106 +88,5 @@ extension KeychainClient {
     enum KeychainError: Swift.Error {
         case resultNotData
         case unhandledError(status: OSStatus)
-    }
-}
-//struct blah {
-//    static func get(_ item: Item) throws -> String? {
-//        var result: CFTypeRef?
-//
-//        guard case errSecSuccess = SecItemCopyMatching(getQuery(for: item), &result) else {
-//            return nil
-//        }
-//        guard let data = result as? Data else {
-//            throw KeychainError.resultNotData
-//        }
-//
-//        return String(data: data, encoding: .utf8)
-//    }
-//
-//    static func remove(_ item: Item) throws {
-//        guard resultExists(for: item) else {
-//            return
-//        }
-//
-//        let status = SecItemDelete(item.baseQuery as CFDictionary)
-//
-//        if status != errSecSuccess {
-//            throw KeychainError.unhandledError(status: status)
-//        }
-//    }
-//
-//    static func set(_ value: String, for item: Item) throws {
-//        let status: OSStatus
-//
-//        if resultExists(for: item) {
-//            status = SecItemUpdate(
-//                item.baseQuery as CFDictionary,
-//                querySegmentForUpdate(for: value) as CFDictionary
-//            )
-//        } else {
-//            status = SecItemAdd(insertQuery(for: item, value: value), nil)
-//        }
-//        if status != errSecSuccess {
-//            throw KeychainError.unhandledError(status: status)
-//        }
-//    }
-//
-//    private static func getQuery(for item: Item) -> CFDictionary {
-//        item.baseQuery
-//            .merging([
-//                kSecReturnData: true,
-//                kSecMatchLimit: kSecMatchLimitOne,
-//            ]) { $1 } as CFDictionary
-//    }
-//
-//    private static func resultExists(for item: Item) -> Bool {
-//        SecItemCopyMatching(item.baseQuery as CFDictionary, nil) == errSecSuccess
-//    }
-//
-//    private static func insertQuery(for item: Item, value: String) -> CFDictionary {
-//        item.baseQuery
-//            .merging(querySegmentForUpdate(for: value)) { $1 } as CFDictionary
-//    }
-//
-//    private static func querySegmentForUpdate(for value: String) -> [CFString: Any] {
-//        [kSecValueData: Data(value.utf8)]
-//    }
-//}
-
-struct KeychainClient {
-    private let getItem: (Self, Item) throws -> String?
-
-    private let setValueForItem: (Self, String, Item) throws -> Void
-
-    private let removeItem: (Self, Item) throws -> Void
-
-    private let resultExists: (Item) -> Bool
-
-    init(
-        getItem: @escaping (KeychainClient, KeychainClient.Item) throws -> String?,
-        setValueForItem: @escaping (KeychainClient, String, KeychainClient.Item) throws -> Void,
-        removeItem: @escaping (KeychainClient, KeychainClient.Item) throws -> Void,
-        resultExists: @escaping (KeychainClient.Item) -> Bool
-    ) {
-        self.getItem = getItem
-        self.setValueForItem = setValueForItem
-        self.removeItem = removeItem
-        self.resultExists = resultExists
-    }
-
-    func get(_ item: Item) throws -> String? {
-        try getItem(self, item)
-    }
-
-    func set(_ value: String, for item: Item) throws {
-        try setValueForItem(self, value, item)
-    }
-
-    func remove(_ item: Item) throws {
-        try removeItem(self, item)
-    }
-
-    func resultExists(for item: Item) -> Bool {
-        resultExists(item)
     }
 }
