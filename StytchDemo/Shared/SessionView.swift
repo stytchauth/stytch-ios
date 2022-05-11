149090475805c51752c9af762544e200f3e8a152
Dan Loman <daniel.h.loman@gmail.com>
2022-05-11 16:45:15 -0700

    Polish SesssionView

--- StytchDemo/Shared/SessionView.swift
+++ StytchDemo/Shared/SessionView.swift
@@ -19,26 +19,30 @@ struct SessionView: View {
             Text("Started at: " + session.startedAt.formatted(date: .abbreviated, time: .shortened))
             Text("Expires at: " + session.expiresAt.formatted(date: .abbreviated, time: .shortened))
             Text("User agent: " + session.attributes.userAgent)
-            Button("Fetch index", action: {
-                var request: URLRequest = .init(url: hostUrl)
-                StytchClient.sessions.sessionToken.map { request.addValue($0.value, forHTTPHeaderField: "X-Stytch-Token") }
+            HStack {
+                Spacer()
+                Button("Fetch host url index", action: {
+                    var request: URLRequest = .init(url: hostUrl)
+                    StytchClient.sessions.sessionToken.map { request.addValue($0.value, forHTTPHeaderField: "X-Stytch-Token") }
 
-                let task = URLSession.shared.dataTask(
-                    with: request,
-                    completionHandler: { data, _, _ in
-                        print(data.flatMap { String(data: $0, encoding: .utf8) } ?? "no data")
+                    let task = URLSession.shared.dataTask(
+                        with: request,
+                        completionHandler: { data, _, _ in
+                            print(data.flatMap { String(data: $0, encoding: .utf8) } ?? "no data")
+                        }
+                    )
+
+                    print("Request: ", request)
+                    print("Original request headers: ", task.originalRequest?.allHTTPHeaderFields ?? [])
+                    Task {
+                        print("Cookies for task: ", await HTTPCookieStorage.shared.cookies(for: task) ?? [])
                     }
-                )
 
-                print("Request: ", request)
-                print("Original request headers: ", task.originalRequest?.allHTTPHeaderFields ?? [])
-                print("Cookies for url: ", HTTPCookieStorage.shared.cookies(for: request.url!) ?? [])
-                Task {
-                    print("Cookies for task: ", await HTTPCookieStorage.shared.cookies(for: task) ?? [])
-                }
-
-                task.resume()
-            })
-        }
+                    task.resume()
+                })
+                .buttonStyle(.borderedProminent)
+                Spacer()
+            }
+        }.padding()
     }
 }
