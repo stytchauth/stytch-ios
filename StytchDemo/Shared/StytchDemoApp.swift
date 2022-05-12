573aca68f78e741af6fe1450b034e2a08a3664eb
Dan Loman <daniel.h.loman@gmail.com>
2022-05-12 08:13:06 -0700

    Add minHeight for the ContentView

--- StytchDemo/Shared/StytchDemoApp.swift
+++ StytchDemo/Shared/StytchDemoApp.swift
@@ -16,6 +16,7 @@ struct StytchDemoApp: App {
                 }
             }
             .padding()
+            .frame(minHeight: 250)
             .task {
                 StytchClient.configure(
                     publicToken: "public-token-test-9e306f84-4f6a-4c23-bbae-abd27bcb90ba", // TODO: extract this token
