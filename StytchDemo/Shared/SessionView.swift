ad08cd24367b37261aa869f54994c1214980796d
Dan Loman <daniel.h.loman@gmail.com>
2022-05-14 15:28:58 -0700

    Tweak SessionView

--- StytchDemo/Shared/SessionView.swift
+++ StytchDemo/Shared/SessionView.swift
@@ -8,20 +8,13 @@ struct SessionView: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text("User ID: " + session.userId)
+            Text("Factors:").bold()
             ForEach(session.authenticationFactors, id: \.lastAuthenticatedAt) { factor in
-                switch factor.deliveryMethod {
-                case let .email(email):
-                    Text("Factor type: email")
-                    Text("Factor ID: \(email.emailId)")
-                    Text(email.emailAddress)
-                case let .sms(sms):
-                    Text("Factor type: email")
-                    Text("Factor ID: \(sms.phoneId)")
-                    Text(sms.phoneNumber)
-                default:
-                    EmptyView()
-                }
+                Text("Factor type: \(factor.deliveryMethod.factorType)")
+                Text("Factor ID: \(factor.deliveryMethod.factorId)")
+                Text(factor.deliveryMethod.factorValue)
             }
+            .padding([.leading], 8)
             Text("Session ID: " + session.sessionId)
             Text("Started at: " + session.startedAt.formatted(date: .abbreviated, time: .shortened))
             Text("Expires at: " + session.expiresAt.formatted(date: .abbreviated, time: .shortened))
@@ -50,6 +43,79 @@ struct SessionView: View {
                 .buttonStyle(.borderedProminent)
                 Spacer()
             }
+        }
+    }
+}
+
+private extension Session.AuthenticationFactor.DeliveryMethod {
+    var factorType: String {
+        switch self {
+        case .authenticatorApp:
+            return "Authenticator App"
+        case .recoveryCode:
+            return "Recovery Code"
+        case .email:
+            return "Email"
+        case .sms:
+            return "SMS"
+        case .whatsapp:
+            return "WhatsApp"
+        case .oauthGoogle:
+            return "OauthGoogle"
+        case .oauthApple:
+            return "OauthApple"
+        case .oauthGithub:
+            return "OauthGithub"
+        case .oauthMicrosoft:
+            return "OauthMicrosoft"
+        case .webauthnRegistration:
+            return "WebAuthN"
+        }
+    }
+
+    var factorId: String {
+        switch self {
+        case let .authenticatorApp(value):
+            return value.totpId
+        case let .recoveryCode(value):
+            return value.totpRecoveryCodeId
+        case let .email(value):
+            return value.emailId
+        case let .sms(value), let .whatsapp(value):
+            return value.phoneId
+        case let .oauthGoogle(value):
+            return value.id
+        case let .oauthApple(value):
+            return value.id
+        case let .oauthGithub(value):
+            return value.id
+        case let .oauthMicrosoft(value):
+            return value.id
+        case let .webauthnRegistration(value):
+            return value.webauthnRegistrationId
+        }
+    }
+
+    var factorValue: String {
+        switch self {
+        case .authenticatorApp:
+            return "THE CODE"
+        case .recoveryCode:
+            return "THE CODE"
+        case let .email(value):
+            return value.emailAddress
+        case let .sms(value), let .whatsapp(value):
+            return value.phoneNumber
+        case let .oauthGoogle(value):
+            return value.providerSubject
+        case let .oauthApple(value):
+            return value.providerSubject
+        case let .oauthGithub(value):
+            return value.providerSubject
+        case let .oauthMicrosoft(value):
+            return value.providerSubject
+        case let .webauthnRegistration(value):
+            return value.domain.absoluteString
         }
     }
 }
