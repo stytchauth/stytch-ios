e9159ae2ed420842362044b3a2f926ede3f89a62
Dan Loman <daniel.h.loman@gmail.com>
2022-05-15 08:31:11 -0700

    Verify the URLRequest vs the curlString

--- Tests/StytchCoreTests/StytchCoreTestCase.swift
+++ Tests/StytchCoreTests/StytchCoreTestCase.swift
@@ -1,5 +1,5 @@
+import Networking
 import XCTest
-@testable import Networking
 @testable import StytchCore
 
 final class StytchCoreTestCase: XCTestCase {
@@ -71,14 +71,9 @@ final class StytchCoreTestCase: XCTestCase {
             dataTaskClient: .mock(
                 returning: .success(data),
                 verifyingRequest: { request in
-                    XCTAssertEqual(
-                        request.curlString,
-                        [
-                            "curl \"https://web.stytch.com/sdk/v1/magic_links/email/login_or_create\"",
-                            "-X POST",
-                            "-d '{\"email\":\"asdf@stytch.com\",\"signup_magic_link_url\":\"https:\\/\\/myapp.com\\/signup\",\"login_magic_link_url\":\"https:\\/\\/myapp.com\\/login\",\"login_expiration_minutes\":30,\"signup_expiration_minutes\":30}'"
-                        ].joined(separator: " \\\n\t")
-                    )
+                    XCTAssertEqual(request.url?.absoluteString, "https://web.stytch.com/sdk/v1/magic_links/email/login_or_create")
+                    XCTAssertEqual(request.httpMethod, "POST")
+                    XCTAssertEqual(request.httpBody, Data("{\"email\":\"asdf@stytch.com\",\"signup_magic_link_url\":\"https:\\/\\/myapp.com\\/signup\",\"login_magic_link_url\":\"https:\\/\\/myapp.com\\/login\",\"login_expiration_minutes\":30,\"signup_expiration_minutes\":30}".utf8))
                 }
             )
         )
@@ -105,14 +100,9 @@ final class StytchCoreTestCase: XCTestCase {
             dataTaskClient: .mock(
                 returning: .success(data),
                 verifyingRequest: { request in
-                    XCTAssertEqual(
-                        request.curlString,
-                        [
-                            "curl \"https://web.stytch.com/sdk/v1/magic_links/authenticate\"",
-                            "-X POST",
-                            "-d '{\"token\":\"12345\",\"session_duration_minutes\":15}'"
-                        ].joined(separator: " \\\n\t")
-                    )
+                    XCTAssertEqual(request.url?.absoluteString, "https://web.stytch.com/sdk/v1/magic_links/authenticate")
+                    XCTAssertEqual(request.httpMethod, "POST")
+                    XCTAssertEqual(request.httpBody, Data("{\"token\":\"12345\",\"session_duration_minutes\":15}".utf8))
                 }
             )
         )
