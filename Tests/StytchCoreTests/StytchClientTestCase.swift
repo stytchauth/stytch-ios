70bb3b6674f9acb17960f6df6104b6061407d41f
Dan Loman <daniel.h.loman@gmail.com>
2022-05-16 07:52:42 -0700

    Lint

--- Tests/StytchCoreTests/StytchClientTestCase.swift
+++ Tests/StytchCoreTests/StytchClientTestCase.swift
@@ -8,7 +8,7 @@ final class StytchClientTestCase: BaseTestCase {
         Current.networkingClient = .mock(verifyingRequest: { request = $0 }, returning: .success(data))
         // Configure the client after setting the networking client, so the headers are passed to the request
         StytchClient.configure(publicToken: "xyz", hostUrl: try XCTUnwrap(URL(string: "https://myapp.com")))
-        let expectation = self.expectation(description: "request_complete")
+        let expectation = expectation(description: "request_complete")
         var response: String?
         StytchClient.get(endpoint: .init(path: "doesnt_matter")) { (result: Result<String, Error>) in
             switch result {
@@ -29,7 +29,7 @@ final class StytchClientTestCase: BaseTestCase {
             [
                 "Content-Type": "application/json",
                 "X-SDK-Client": "eyJhcHAiOnsiaWRlbnRpZmllciI6ImNvbS5hcHBsZS5kdC54Y3Rlc3QudG9vbCJ9LCJzZGsiOnsidmVyc2lvbiI6IjAuMC4xIiwiaWRlbnRpZmllciI6InN0eXRjaC1zd2lmdCJ9LCJvcyI6eyJ2ZXJzaW9uIjoiMTIuMi4xIiwiaWRlbnRpZmllciI6Im1hY29zIn0sImRldmljZSI6eyJtb2RlbCI6Im1hY09TIiwic2NyZWVuX3NpemUiOiIoMTUxMi4wLDk4Mi4wKSJ9fQ==",
-                "Authorization": "Basic eHl6Onh5eg=="
+                "Authorization": "Basic eHl6Onh5eg==",
             ]
         )
 
