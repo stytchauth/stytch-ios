b8a4dd12dfac8913f33ce90582a603d63bf6a8a0
Dan Loman <dan@stytch.com>
2022-05-21 14:29:20 -0700

    Start to set KeychainClient up to handle biometrics

--- Sources/StytchCore/KeychainClient/KeychainClient+Live.swift
+++ Sources/StytchCore/KeychainClient/KeychainClient+Live.swift
@@ -2,47 +2,118 @@ import Foundation
 import Security
 
 extension KeychainClient {
-    static let live: Self = .init(
-        getItem: { item in
-            var result: CFTypeRef?
+    static let live: Self = .init { item in
+        var result: CFTypeRef?
 
-            guard case errSecSuccess = SecItemCopyMatching(item.getQuery, &result) else {
-                return nil
-            }
-            guard let data = result as? Data else {
-                throw KeychainError.resultNotData
-            }
-
-            return String(data: data, encoding: .utf8)
-        },
-        setValueForItem: { client, value, item in
-            let status: OSStatus
-
-            if client.resultExists(for: item) {
-                status = SecItemUpdate(
-                    item.baseQuery as CFDictionary,
-                    item.querySegmentForUpdate(for: value) as CFDictionary
-                )
-            } else {
-                status = SecItemAdd(item.insertQuery(value: value), nil)
-            }
-            if status != errSecSuccess {
-                throw KeychainError.unhandledError(status: status)
-            }
-        },
-        removeItem: { client, item in
-            guard client.resultExists(for: item) else {
-                return
-            }
-
-            let status = SecItemDelete(item.baseQuery as CFDictionary)
-
-            if status != errSecSuccess {
-                throw KeychainError.unhandledError(status: status)
-            }
-        },
-        resultExists: { item in
-            SecItemCopyMatching(item.baseQuery as CFDictionary, nil) == errSecSuccess
+        guard case errSecSuccess = SecItemCopyMatching(item.getQuery, &result) else {
+            return nil
         }
-    )
+        guard let data = result as? Data else {
+            throw KeychainError.resultNotData
+        }
+
+        return String(data: data, encoding: .utf8)
+    } setValueForItem: { client, value, item in
+        let status: OSStatus
+
+        if client.resultExists(for: item) {
+            status = SecItemUpdate(
+                item.baseQuery as CFDictionary,
+                item.querySegmentForUpdate(for: value) as CFDictionary
+            )
+        } else {
+            status = SecItemAdd(item.insertQuery(value: value), nil)
+        }
+        if status != errSecSuccess {
+            throw KeychainError.unhandledError(status: status)
+        }
+    } removeItem: { client, item in
+        guard client.resultExists(for: item) else {
+            return
+        }
+
+        let status = SecItemDelete(item.baseQuery as CFDictionary)
+
+        if status != errSecSuccess {
+            throw KeychainError.unhandledError(status: status)
+        }
+    } resultExists: { item in
+        SecItemCopyMatching(item.baseQuery as CFDictionary, nil) == errSecSuccess
+    } publicKeyForItem: { client, item in
+        if let publicKey = try client.fetchKeyForItem(item, .public) {
+            return try .init(publicKey)
+        }
+
+        guard case let .keyPair(appStatusOption) = item.kind else { throw KeychainError.resultNotData } // FIXME: - use different error type
+
+        var error: Unmanaged<CFError>?
+
+        let accessControl: SecAccessControlCreateFlags
+
+        if #available(iOS 11.3, macOS 10.13.4, tvOS 11.3, watchOS 4.3, *) {
+            accessControl = [.biometryCurrentSet]
+        } else {
+            accessControl = [.touchIDCurrentSet]
+        }
+
+        guard let accessControl = SecAccessControlCreateWithFlags(nil, appStatusOption.value, accessControl, &error) else {
+            throw error.toError() ?? KeychainError.resultNotData // FIXME: - (message: "Unable to create access control")
+        }
+
+        let query = item.createKeyPairQuery(accessControl: accessControl)
+
+        guard let privateKey = SecKeyCreateRandomKey(query as CFDictionary, &error) else {
+            throw error.toError() ?? KeychainError.notSecKey // FIXME: - (message: "Unable to create private key")
+        }
+
+        guard let publicKey = SecKeyCopyPublicKey(privateKey) else {
+            throw KeychainError.notSecKey // FIXME: - (message: "Unable to generate public key from private key")
+        }
+
+        return try .init(publicKey)
+    } fetchKeyForItem: { item, keyType in
+        let query = item.getKeyPairQuery(keyType: keyType)
+
+        var result: AnyObject?
+
+        let queryStatus = SecItemCopyMatching(query as CFDictionary, &result)
+
+        if queryStatus == errSecItemNotFound {
+            return nil
+        }
+
+        guard queryStatus == errSecSuccess else {
+            throw KeychainError.unhandledError(status: queryStatus)
+        }
+
+        guard let result = result, let castedKey = result as? SecKey?, let key = castedKey else {
+            throw KeychainError.notSecKey
+        }
+
+        return key
+    } signChallenge: { client, challenge, item, algorithm in
+        var error: Unmanaged<CFError>?
+
+        guard let privateKey = try client.fetchKeyForItem(item, .private) else {
+            throw KeychainError.notSecKey // FIXME: - (message: "Unable to find private key")
+        }
+
+        guard
+            case let algorithm: SecKeyAlgorithm = .init(rawValue: algorithm as CFString),
+            SecKeyIsAlgorithmSupported(privateKey, .sign, algorithm)
+        else {
+            throw KeychainError.notSecKey // FIXME: - correct error
+        }
+
+        guard let signatureData = SecKeyCreateSignature(
+            privateKey,
+            algorithm,
+            Data(challenge.utf8) as CFData,
+            &error
+        ) as? Data else {
+            throw error.toError() ?? KeychainError.notSecKey // FIXME: - (message: "Unable to sign challenge")
+        }
+
+        return signatureData.base64EncodedString()
+    }
 }
--- Sources/StytchCore/KeychainClient/KeychainClient.swift
+++ Sources/StytchCore/KeychainClient/KeychainClient.swift
@@ -9,16 +9,28 @@ struct KeychainClient {
 
     private let resultExists: (Item) -> Bool
 
+    private let publicKeyForItem: (Self, Item) throws -> PublicKey?
+
+    let fetchKeyForItem: (Item, KeyPairType) throws -> SecKey?
+
+    private let signChallenge: (Self, String, Item, String) throws -> String
+
     init(
-        getItem: @escaping (KeychainClient.Item) throws -> String?,
-        setValueForItem: @escaping (KeychainClient, String, KeychainClient.Item) throws -> Void,
-        removeItem: @escaping (KeychainClient, KeychainClient.Item) throws -> Void,
-        resultExists: @escaping (KeychainClient.Item) -> Bool
+        getItem: @escaping (Item) throws -> String?,
+        setValueForItem: @escaping (Self, String, Item) throws -> Void,
+        removeItem: @escaping (Self, Item) throws -> Void,
+        resultExists: @escaping (Item) -> Bool,
+        publicKeyForItem: @escaping (Self, Item) throws -> PublicKey?,
+        fetchKeyForItem: @escaping (Item, KeyPairType) throws -> SecKey?,
+        signChallenge: @escaping (Self, String, Item, String) throws -> String
     ) {
         self.getItem = getItem
         self.setValueForItem = setValueForItem
         self.removeItem = removeItem
         self.resultExists = resultExists
+        self.publicKeyForItem = publicKeyForItem
+        self.fetchKeyForItem = fetchKeyForItem
+        self.signChallenge = signChallenge
     }
 
     func get(_ item: Item) throws -> String? {
@@ -36,12 +48,21 @@ struct KeychainClient {
     func resultExists(for item: Item) -> Bool {
         resultExists(item)
     }
+
+    func publicKey(for item: Item) throws -> PublicKey? {
+        try publicKeyForItem(self, item)
+    }
+
+    func sign(challenge: String, for item: Item, using algorithm: String) throws -> String {
+        try signChallenge(self, challenge, item, algorithm)
+    }
 }
 
 extension KeychainClient {
     struct Item {
         enum Kind {
             case token
+            case keyPair(AppStatusOption)
         }
 
         var kind: Kind
@@ -52,19 +73,32 @@ extension KeychainClient {
             var query: [CFString: Any] = [
                 kSecClass: secClass,
                 kSecAttrAccount: name,
+                kSecAttrApplicationLabel: Bundle.main.bundleIdentifier ?? "com.stytch.StytchCore", // FIXME: - confirm this is desired/handles app groups
             ]
             if #available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *) {
                 query[kSecUseDataProtectionKeychain] = true
+            }
+            if case .keyPair = kind {
+                query[kSecAttrKeyType] = kSecAttrKeyTypeEC // TODO: confirm this vs ECPrimeRandom
             }
             return query
         }
 
         var getQuery: CFDictionary {
-            baseQuery
-                .merging([
+            let getQuerySegment: [CFString: Any]
+
+            switch kind {
+            case .token:
+                getQuerySegment = [
                     kSecReturnData: true,
                     kSecMatchLimit: kSecMatchLimitOne,
-                ]) { $1 } as CFDictionary
+                ]
+            case .keyPair:
+                getQuerySegment = [kSecReturnRef: true]
+            }
+
+            return baseQuery
+                .merging(getQuerySegment) { $1 } as CFDictionary
         }
 
         func insertQuery(value: String) -> CFDictionary {
@@ -75,16 +109,106 @@ extension KeychainClient {
             [kSecValueData: Data(value.utf8)]
         }
 
+        func createKeyPairQuery(accessControl: SecAccessControl) -> CFDictionary {
+            baseQuery.merging([
+                kSecAttrKeySizeInBits: 2048, // TODO: - confirm key size
+                kSecAttrIsPermanent: true,
+                kSecPublicKeyAttrs: [
+                    kSecAttrApplicationTag: KeyPairType.public.rawValue,
+                ],
+                kSecPrivateKeyAttrs: [
+                    kSecAttrApplicationTag: KeyPairType.private.rawValue,
+                    kSecAttrAccessControl: accessControl, // FIXME: - messed up on ios 15 simulator
+                ],
+            ]) { $1 } as CFDictionary
+        }
+
+        func getKeyPairQuery(keyType: KeyPairType) -> CFDictionary {
+            baseQuery.merging([kSecAttrApplicationTag: keyType.rawValue]) { $1 } as CFDictionary
+        }
+
         private var secClass: CFString {
             switch kind {
             case .token:
                 return kSecClassGenericPassword
+            case .keyPair:
+                return kSecClassKey
             }
         }
     }
 
     enum KeychainError: Swift.Error {
         case resultNotData
+        case notSecKey
         case unhandledError(status: OSStatus)
+    }
+}
+
+extension KeychainClient {
+    public struct PublicKey {
+        public let base64Encoded: String
+        let secKey: SecKey
+
+        public init(rawValue: String) throws {
+            let options: [String: Any] = [
+                kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+                kSecAttrKeyClass as String: kSecAttrKeyClassPublic,
+            ]
+
+            var error: Unmanaged<CFError>?
+
+            guard let secKey = SecKeyCreateWithData(Data(rawValue.utf8) as CFData, options as CFDictionary, &error) else {
+                throw error.toError() ?? KeychainError.notSecKey // FIXME: - (message: "Unable to create key from external data")
+            }
+
+            base64Encoded = rawValue
+            self.secKey = secKey
+        }
+
+        public init(_ publicKey: SecKey) throws {
+            var error: Unmanaged<CFError>?
+
+            guard let externalRepresentationData = SecKeyCopyExternalRepresentation(publicKey, &error) as? Data else {
+                throw error.toError() ?? KeychainError.notSecKey // FIXME: - (message: "Unable to copy external public key representation")
+            }
+
+            base64Encoded = externalRepresentationData.base64EncodedString()
+            secKey = publicKey
+        }
+    }
+
+    public enum AppStatusOption {
+        case foreground
+        case background
+
+        var value: CFString {
+            switch self {
+            case .background:
+                return kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
+            case .foreground:
+                return kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly
+            }
+        }
+    }
+
+    enum KeyPairType: String {
+        case `private`
+        case `public`
+    }
+}
+
+extension Optional where Wrapped == Unmanaged<CFError> {
+    func throwIfPresent() throws {
+        try map { throw $0.asError }
+    }
+
+    func toError() -> Error? {
+        self?.asError
+    }
+}
+
+extension Unmanaged where Instance == CFError {
+    var asError: Error {
+        takeRetainedValue() as Error
     }
 }
