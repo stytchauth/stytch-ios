b33b8c71decdafff0cdc42a1fb904389df1dabf7
Dan Loman <dan@stytch.com>
2022-05-26 17:24:14 -0700

    Add documentation

--- Sources/StytchCore/PollingClient+Sessions.swift
+++ Sources/StytchCore/PollingClient+Sessions.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 extension PollingClient {
+    /// A polling client which tries to complete every 3 minutes, and will retry (w/ exponential delays) upon errors, for up to a total of approximately 124 seconds.
     static let sessions: PollingClient = .init(interval: 180, maxRetries: 5, queue: .sessionsPolling) { onSuccess, onFailure in
         StytchClient.sessions.authenticate(parameters: .init()) { result in
             switch result {
