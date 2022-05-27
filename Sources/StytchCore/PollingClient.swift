e65042b578cfda14c08697cf3cd3edf0efbe15f0
Dan Loman <dan@stytch.com>
2022-05-26 17:24:05 -0700

    Make the Client/Retrier slightly more testable. Add jitter

--- Sources/StytchCore/PollingClient.swift
+++ Sources/StytchCore/PollingClient.swift
@@ -4,14 +4,26 @@ final class PollingClient {
     private let interval: TimeInterval
     private let maxRetries: UInt
     private let queue: DispatchQueue
+    private let onInterval: (PollingClient, TimeInterval, @escaping () -> Void) -> Void
     private let task: Task
     private var retrier: Retrier?
     private var timer: Timer?
 
-    init(interval: TimeInterval, maxRetries: UInt, queue: DispatchQueue, task: @escaping Task) {
+    init(
+        interval: TimeInterval,
+        maxRetries: UInt,
+        queue: DispatchQueue,
+        onInterval: @escaping (PollingClient, TimeInterval, @escaping () -> Void) -> Void = { client, interval, task in
+            let timer = Timer(timeInterval: interval, repeats: true, block: { _ in task() })
+            client.timer = timer
+            RunLoop.main.add(timer, forMode: .common)
+        },
+        task: @escaping Task
+    ) {
         self.interval = interval
         self.maxRetries = maxRetries
         self.queue = queue
+        self.onInterval = onInterval
         self.task = task
     }
 
@@ -20,16 +32,12 @@ final class PollingClient {
         retrier?.cancel()
         retrier = nil
 
-        let timer = Timer(timeInterval: interval, repeats: true) { [weak self] _ in
+        onInterval(self, interval) { [weak self] in
             guard let self = self else { return }
             self.retrier?.cancel()
             self.retrier = .init(maxRetries: self.maxRetries, queue: self.queue, task: self.task)
             self.retrier?.attempt()
         }
-
-        self.timer = timer
-
-        RunLoop.main.add(timer, forMode: .common)
     }
 
     func stopPolling() {
@@ -50,11 +58,18 @@ private extension PollingClient {
         private let maxRetries: UInt
         private let queue: DispatchQueue
         private var isCancelled: Bool = false
+        private let asyncAfter: (DispatchQueue, DispatchTime, @escaping () -> Void) -> Void
         private let task: Task
 
-        init(maxRetries: UInt, queue: DispatchQueue, task: @escaping Task) {
+        init(
+            maxRetries: UInt,
+            queue: DispatchQueue,
+            asyncAfter: @escaping (DispatchQueue, DispatchTime, @escaping () -> Void) -> Void = { $0.asyncAfter(deadline: $1, execute: $2) },
+            task: @escaping Task
+        ) {
             self.maxRetries = maxRetries
             self.queue = queue
+            self.asyncAfter = asyncAfter
             self.task = task
         }
 
@@ -67,6 +82,8 @@ private extension PollingClient {
                 self.currentRetryValue += 1
                 self.attempt(success: success, failure: failure)
             }
+            // We'll set the due time to: 2 seconds from now * 2^n +/- jitter
+            let delay: (UInt) -> DispatchTime = { .now() + 2 * pow(2, Double($0 - 1)) + Double.random(in: (-0.0175...0.0175)) }
             let wrappedTask = { [weak self] in
                 guard let self = self, !self.isCancelled else { return }
                 self.task(success, failureWrapper)
@@ -74,7 +91,7 @@ private extension PollingClient {
             if currentRetryValue == 0 {
                 wrappedTask()
             } else {
-                queue.asyncAfter(deadline: .now() + pow(2, Double(currentRetryValue - 1)), execute: wrappedTask)
+                asyncAfter(queue, delay(currentRetryValue), wrappedTask)
             }
         }
 
