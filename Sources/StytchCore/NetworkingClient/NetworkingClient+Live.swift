ff49e9a840529840380b052de576fbc91f1d5144
Dan Loman <dan@stytch.com>
2022-05-26 20:37:16 -0700

    Revert NetworkingClient fake behavior

--- Sources/StytchCore/NetworkingClient/NetworkingClient+Live.swift
+++ Sources/StytchCore/NetworkingClient/NetworkingClient+Live.swift
@@ -1,34 +1,26 @@
 import Foundation
 
-var count = 0
-
 extension NetworkingClient {
     static let live: NetworkingClient = {
         let session: URLSession = .init(configuration: .default)
         return .init { request, completion in
-            if count > 1 {
-                completion(.failure(Error.missingData))
-                return .init(dataTask: nil)
-            } else {
-                let task = session.dataTask(with: request) { data, response, error in
-                    if let error = error {
-                        completion(.failure(error))
-                        return
-                    }
-                    guard let data = data else {
-                        completion(.failure(Error.missingData))
-                        return
-                    }
-                    guard let response = response as? HTTPURLResponse else {
-                        completion(.failure(Error.nonHttpResponse))
-                        return
-                    }
-                    completion(.success((data, response)))
+            let task = session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
                 }
-                task.resume()
-                count += 1
-                return .init(dataTask: task)
+                guard let data = data else {
+                    completion(.failure(NetworkingClient.Error.missingData))
+                    return
+                }
+                guard let response = response as? HTTPURLResponse else {
+                    completion(.failure(NetworkingClient.Error.nonHttpResponse))
+                    return
+                }
+                completion(.success((data, response)))
             }
+            task.resume()
+            return .init(dataTask: task)
         }
     }()
 }
