6f6920dbe42b2513324f221ac2140dd0d632b3d7
Dan Loman <dan@stytch.com>
2022-05-30 06:36:01 -0700

    Revert "[docs] Update async docs"
    This reverts commit 89dfd174a3083ff62205c48cab8f504f6b344f56.

--- Sources/StytchCore/Generated/StytchClient.Sessions+AsyncVariants.generated.swift
+++ Sources/StytchCore/Generated/StytchClient.Sessions+AsyncVariants.generated.swift
@@ -8,7 +8,7 @@ import Combine
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
 public extension StytchClient.Sessions {
-    /// Wraps Stytch's [authenticate](https://stytch.com/docs/api/session-auth) Session endpoint and validates that the session issued to the user is still valid, returning both an opaque sessionToken and sessionJwt for this session. The sessionJwt will have a fixed lifetime of five minutes regardless of the underlying session duration, though it will be refreshed automatically in the background after a successful authentication.
+    /// Wraps Stytch's [authenticate](https://stytch.com/docs/api/session-auth) Session endpoint and validates that the session issued to the user is still valid.
     func authenticate(parameters: AuthenticateParameters) -> AnyPublisher<SessionsAuthenticateResponse, Error> {
         return Deferred { 
             Future({ promise in
@@ -24,7 +24,7 @@ public extension StytchClient.Sessions {
 #if compiler(>=5.5) && canImport(_Concurrency)
 public extension StytchClient.Sessions {
     #if compiler(>=5.5.2)
-    /// Wraps Stytch's [authenticate](https://stytch.com/docs/api/session-auth) Session endpoint and validates that the session issued to the user is still valid, returning both an opaque sessionToken and sessionJwt for this session. The sessionJwt will have a fixed lifetime of five minutes regardless of the underlying session duration, though it will be refreshed automatically in the background after a successful authentication.
+    /// Wraps Stytch's [authenticate](https://stytch.com/docs/api/session-auth) Session endpoint and validates that the session issued to the user is still valid.
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
     func authenticate(parameters: AuthenticateParameters) async throws -> SessionsAuthenticateResponse {
         try await withCheckedThrowingContinuation { continuation in
@@ -32,7 +32,7 @@ public extension StytchClient.Sessions {
         }
     }
     #else
-    /// Wraps Stytch's [authenticate](https://stytch.com/docs/api/session-auth) Session endpoint and validates that the session issued to the user is still valid, returning both an opaque sessionToken and sessionJwt for this session. The sessionJwt will have a fixed lifetime of five minutes regardless of the underlying session duration, though it will be refreshed automatically in the background after a successful authentication.
+    /// Wraps Stytch's [authenticate](https://stytch.com/docs/api/session-auth) Session endpoint and validates that the session issued to the user is still valid.
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
     func authenticate(parameters: AuthenticateParameters) async throws -> SessionsAuthenticateResponse {
         try await withCheckedThrowingContinuation { continuation in
@@ -51,7 +51,7 @@ import Combine
 
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
 public extension StytchClient.Sessions {
-    /// Wraps Stytch's [revoke](https://stytch.com/docs/api/session-revoke) Session endpoint and revokes the user's current session. This method should be used to log out a user. A successful revocation will terminate session-refresh polling.
+    /// Wraps Stytch's [revoke](https://stytch.com/docs/api/session-revoke) Session endpoint and revokes the user's current session. This method should be used to log out a user.
     func revoke() -> AnyPublisher<SessionsRevokeResponse, Error> {
         return Deferred { 
             Future({ promise in
@@ -67,7 +67,7 @@ public extension StytchClient.Sessions {
 #if compiler(>=5.5) && canImport(_Concurrency)
 public extension StytchClient.Sessions {
     #if compiler(>=5.5.2)
-    /// Wraps Stytch's [revoke](https://stytch.com/docs/api/session-revoke) Session endpoint and revokes the user's current session. This method should be used to log out a user. A successful revocation will terminate session-refresh polling.
+    /// Wraps Stytch's [revoke](https://stytch.com/docs/api/session-revoke) Session endpoint and revokes the user's current session. This method should be used to log out a user.
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
     func revoke() async throws -> SessionsRevokeResponse {
         try await withCheckedThrowingContinuation { continuation in
@@ -75,7 +75,7 @@ public extension StytchClient.Sessions {
         }
     }
     #else
-    /// Wraps Stytch's [revoke](https://stytch.com/docs/api/session-revoke) Session endpoint and revokes the user's current session. This method should be used to log out a user. A successful revocation will terminate session-refresh polling.
+    /// Wraps Stytch's [revoke](https://stytch.com/docs/api/session-revoke) Session endpoint and revokes the user's current session. This method should be used to log out a user.
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
     func revoke() async throws -> SessionsRevokeResponse {
         try await withCheckedThrowingContinuation { continuation in
