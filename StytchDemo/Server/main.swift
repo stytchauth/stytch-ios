439e361b5b58c39262d8dddd3d53bc45e9c627ec
Dan Loman <daniel.h.loman@gmail.com>
2022-06-04 07:45:56 -0700

    Format

--- StytchDemo/Server/main.swift
+++ StytchDemo/Server/main.swift
@@ -6,7 +6,8 @@ if ProcessInfo.processInfo.environment["DEMO_SERVER_SCRIPT"] == nil {
 
 var args = ProcessInfo.processInfo.arguments.dropFirst()
 
-guard let port = UInt16(args.removeFirst())
-else { fatalError("Expected an integer value for the port as the first argument") }
+guard let port = args.popFirst().flatMap(UInt16.init) else {
+    fatalError("Expected an integer value for the port as the first argument")
+}
 
 startServer(port: port)
