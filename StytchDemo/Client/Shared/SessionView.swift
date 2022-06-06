f11bf016e450db7cd485600cf996bf63ea682575
Dan Loman <daniel.h.loman@gmail.com>
2022-06-05 23:40:36 -0700

    Remove fetchindex button code

--- StytchDemo/Client/Shared/SessionView.swift
+++ StytchDemo/Client/Shared/SessionView.swift
@@ -23,30 +23,6 @@ struct SessionView: View {
             Text("Started at: " + session.startedAt.formatted(date: .abbreviated, time: .shortened))
             Text("Expires at: " + session.expiresAt.formatted(date: .abbreviated, time: .shortened))
             Text("User agent: " + session.attributes.userAgent)
-//            HStack {
-//                Spacer()
-//                Button("Fetch host url index", action: {
-//                    var request: URLRequest = .init(url: serverUrl)
-//                    StytchClient.sessions.sessionToken.map { request.addValue($0.value, forHTTPHeaderField: "X-Stytch-Token") }
-//
-//                    let task = URLSession.shared.dataTask(
-//                        with: request,
-//                        completionHandler: { data, _, _ in
-//                            print(data.flatMap { String(data: $0, encoding: .utf8) } ?? "no data")
-//                        }
-//                    )
-//
-//                    print("Request: ", request)
-//                    print("Original request headers: ", task.originalRequest?.allHTTPHeaderFields ?? [])
-//                    Task {
-//                        print("Cookies for task: ", await HTTPCookieStorage.shared.cookies(for: task) ?? [])
-//                    }
-//
-//                    task.resume()
-//                })
-//                .buttonStyle(.borderedProminent)
-//                Spacer()
-//            }
         }
     }
 }
