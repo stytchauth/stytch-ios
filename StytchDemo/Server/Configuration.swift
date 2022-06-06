993afa5f05aa03d4ecddc0fc5d2426a59996ece2
Dan Loman <daniel.h.loman@gmail.com>
2022-06-05 22:28:11 -0700

    Add hostUrl to config

--- StytchDemo/Server/Configuration.swift
+++ StytchDemo/Server/Configuration.swift
@@ -10,7 +10,26 @@ let configuration: Configuration = {
 }()
 
 struct Configuration: Decodable {
+    let hostUrl: URL
     let appId: String
 
-    private enum CodingKeys: String, CodingKey { case appId = "APP_ID" }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        guard
+            case let urlString = try container.decode(String.self, forKey: .hostUrl),
+            let url = URL(string: urlString)
+        else {
+            throw DecodingError
+                .dataCorruptedError(forKey: .hostUrl, in: container, debugDescription: "Expected valid URL string")
+        }
+
+        hostUrl = url
+        appId = try container.decode(String.self, forKey: .appId)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case appId = "APP_ID"
+        case hostUrl = "HOST_URL"
+    }
 }
