221d0d3fda0e8f419be1eec8664625858b73d3cb
Dan Loman <daniel.h.loman@gmail.com>
2022-06-05 23:26:40 -0700

    Don't fatalerror

--- StytchDemo/Server/Controllers/Controller.swift
+++ StytchDemo/Server/Controllers/Controller.swift
@@ -5,9 +5,10 @@ protocol Controller {
 }
 
 extension HttpServer.MethodRoute {
-    subscript<T: Controller>(path: String) -> (T) -> () async -> HttpResponse {
-        get { fatalError() }
+    subscript<T: Controller>(path: String) -> ((T) -> () async -> HttpResponse)? {
+        get { nil }
         set {
+            guard let newValue = newValue else { return }
             router.register(method, path: path) { request in
                 await newValue(T(request: request))()
             }
