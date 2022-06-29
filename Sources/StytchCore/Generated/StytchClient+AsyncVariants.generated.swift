80e7a656e62fb1bd373f7ec93860ce138ad27726
Dan Loman <99769393+dan-stytch@users.noreply.github.com>
2022-06-29 11:55:16 -0700

    [build] Update min deployment target to iOS 13 (#56)
    * Revert "Revert "[build] Update min deployment target to iOS 13""
    
    This reverts commit 55b069f9ba32196961bc86df92967e6f50e0509e.
    
    * Update deployment target references and version for podspec

--- Sources/StytchCore/Generated/StytchClient+AsyncVariants.generated.swift
+++ Sources/StytchCore/Generated/StytchClient+AsyncVariants.generated.swift
@@ -6,7 +6,6 @@ import Foundation
 #if canImport(Combine)
 import Combine
 
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
 public extension StytchClient {
     /// This function is provided as a simple convenience handler to be used in your AppDelegate or
     /// SwiftUI App file upon receiving a deeplink URL, e.g. `.onOpenURL {}`.
@@ -30,7 +29,6 @@ public extension StytchClient {
 // MARK: - handle Async/Await
 #if compiler(>=5.5) && canImport(_Concurrency)
 public extension StytchClient {
-    #if compiler(>=5.5.2)
     /// This function is provided as a simple convenience handler to be used in your AppDelegate or
     /// SwiftUI App file upon receiving a deeplink URL, e.g. `.onOpenURL {}`.
     /// If Stytch is able to handle the URL and log the user in, an ``AuthenticateResponse`` will be returned to you asynchronously, with a `sessionDuration` of
@@ -39,27 +37,10 @@ public extension StytchClient {
     ///    - url: A `URL` passed to your application as a deeplink.
     ///    - sessionDuration: The desired session duration in ``Minutes``. Defaults to 30.
     ///  - Returns: A ``DeeplinkHandledStatus`` will be returned asynchronously.
-    @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.2, *)
     static func handle(url: URL, sessionDuration: Minutes = 30) async throws -> DeeplinkHandledStatus {
         try await withCheckedThrowingContinuation { continuation in
             handle(url: url, sessionDuration: sessionDuration, completion: continuation.resume)
         }
     }
-    #else
-    /// This function is provided as a simple convenience handler to be used in your AppDelegate or
-    /// SwiftUI App file upon receiving a deeplink URL, e.g. `.onOpenURL {}`.
-    /// If Stytch is able to handle the URL and log the user in, an ``AuthenticateResponse`` will be returned to you asynchronously, with a `sessionDuration` of
-    /// the length requested here.
-    ///  - Parameters:
-    ///    - url: A `URL` passed to your application as a deeplink.
-    ///    - sessionDuration: The desired session duration in ``Minutes``. Defaults to 30.
-    ///  - Returns: A ``DeeplinkHandledStatus`` will be returned asynchronously.
-    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-    static func handle(url: URL, sessionDuration: Minutes = 30) async throws -> DeeplinkHandledStatus {
-        try await withCheckedThrowingContinuation { continuation in
-            handle(url: url, sessionDuration: sessionDuration, completion: continuation.resume)
-        }
-    }
-    #endif
 }
 #endif
