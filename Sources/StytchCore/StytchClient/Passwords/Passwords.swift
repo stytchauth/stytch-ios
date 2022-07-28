f83ff9eab25deb7c30b7cdb3092347696c998aeb
Dan Loman <dan@stytch.com>
2022-07-28 14:53:42 -0700

    Fix some Passwords docs

--- Sources/StytchCore/StytchClient/Passwords/Passwords.swift
+++ Sources/StytchCore/StytchClient/Passwords/Passwords.swift
@@ -68,7 +68,7 @@ public extension StytchClient {
         /// 1. [zxcvbn](https://github.com/dropbox/zxcvbn)'s strength score is <= 2.
         /// 2. The password is present in the HaveIBeenPwned dataset.
         ///
-        /// This method takes `email` as an optional argument, and if it is passed it will be factored into zxcvbn’s evaluation of the strength of the password. If you do not pass the email, it is possible that the password will evaluate as valid – but will fail with a weak_password error when used in the ``StytchClient/Passwords-swift.struct/create(parameters:)`` method.
+        /// This method takes `email` as an optional argument, and if it is passed it will be factored into zxcvbn’s evaluation of the strength of the password. If you do not pass the email, it is possible that the password will evaluate as valid – but will fail with a weak_password error when used in the ``StytchClient/Passwords-swift.struct/create(parameters:)-3gtlz`` method.
         /// Feedback will be present in the response for any password that does not meet the strength requirements, and mirrors that feedback provided by the zxcvbn library.
         public func strengthCheck(parameters: StrengthCheckParameters) async throws -> StrengthCheckResponse {
             try await router.post(to: .strengthCheck, parameters: parameters)
