145f0febd42839783b76fbd4b860756375425164
Dan Loman <daniel.h.loman@gmail.com>
2022-09-16 15:00:15 -0700

    Clean up biometrics implementation

--- Sources/StytchCore/StytchClient/Biometrics/StytchClient+Biometrics.swift
+++ Sources/StytchCore/StytchClient/Biometrics/StytchClient+Biometrics.swift
@@ -1,15 +1,24 @@
 import Foundation
 
-// NOTE: - Ability to import LocalAuthentication serves as a proxy for supported platforms for biometrics
-#if canImport(LocalAuthentication)
-extension StytchClient {
+public extension StytchClient {
     /// some docs
     struct Biometrics {
         let router: NetworkingRouter<BiometricsRoute>
 
+        private static let biometricRegistrationKey: String = "biometric_registration_available"
+
+        public var registrationAvailable: Bool {
+            Current.defaults.bool(forKey: Self.biometricRegistrationKey)
+        }
+
+        public func removeRegistration() throws {
+            try Current.keychainClient.removeItem(.privateKeyRegistration)
+            Current.defaults.removeObject(forKey: Self.biometricRegistrationKey)
+        }
+
         // sourcery: AsyncAsyncVariants, (NOTE: - must use /// doc comment styling)
         /// some docs
-        public func register(parameters: RegisterParameters) async throws -> BasicResponse {
+        public func register(parameters: RegisterParameters) async throws -> AuthenticateResponseType {
             // Early out if not authenticated
             guard (Current.sessionStorage.sessionToken ?? Current.sessionStorage.sessionJwt) != nil else {
                 throw StytchError.noCurrentSession
@@ -29,7 +38,8 @@ extension StytchClient {
                     signature: Current.cryptoClient.signChallengeWithPrivateKey(
                         startResponse.challenge,
                         privateKey
-                    )
+                    ),
+                    sessionDuration: parameters.sessionDuration
                 )
             )
 
@@ -42,45 +52,30 @@ extension StytchClient {
             try Current.keychainClient.set(
                 key: privateKey,
                 registration: registration,
-                accessPolicy: parameters.accessPolicy.keychainValue,
-                syncingBehavior: parameters.syncingBehavior.keychainValue
+                accessPolicy: parameters.accessPolicy.keychainValue
             )
 
-            return .init(
-                requestId: finishResponse.requestId,
-                statusCode: finishResponse.statusCode
-            )
+            Current.defaults.set(true, forKey: Self.biometricRegistrationKey)
+
+            return finishResponse
         }
 
         // sourcery: AsyncAsyncVariants, (NOTE: - must use /// doc comment styling)
         /// some docs
-        public func authenticate(
-            parameters: AuthenticateParameters,
-            handler: ([Registration]) async -> Registration
-        ) async throws -> AuthenticateResponse {
-            guard case let queryResults = try Current.keychainClient.get(.privateKeyRegistration), !queryResults.isEmpty else {
+        public func authenticate(parameters: AuthenticateParameters) async throws -> AuthenticateResponseType {
+            guard let queryResult: KeychainClient.QueryResult = try Current.keychainClient.get(.privateKeyRegistration).first else {
                 throw StytchError.noBiometricRegistrationsAvailable
             }
 
-            let queryResult: KeychainClient.QueryResult
-
-            if let result = queryResults.first, queryResults.count == 1 {
-                queryResult = result
-            } else {
-                let registrations = queryResults.enumerated().compactMap { index, result in
-                    result.label.map { Registration(userLabel: $0, createdAt: result.createdAt, index: index) }
-                }
-                queryResult = await queryResults[handler(registrations).index]
-            }
             let privateKey = queryResult.data
             let publicKey = try Current.cryptoClient.publicKeyForPrivateKey(privateKey)
 
             let startResponse: AuthenticateStartResponse = try await router.post(
                 to: .authenticate(.start),
-                parameters: AuthenticateStartParameters(publicKey: publicKey, userId: queryResult.account)
+                parameters: AuthenticateStartParameters(publicKey: publicKey)
             )
 
-            // Could use separate concrete type which contains biometric registration id, but this doesn't seem to add value
+            // NOTE: - Could return separate concrete type which deserializes/contains biometric_registration_id, but this doesn't seem to add much value
             return try await router.post(
                 to: .authenticate(.complete),
                 parameters: AuthenticateCompleteParameters(
@@ -88,24 +83,72 @@ extension StytchClient {
                     biometricRegistrationId: startResponse.biometricRegistrationId,
                     sessionDurationMinutes: parameters.sessionDuration
                 )
-            )
+            ) as AuthenticateResponse
         }
     }
 }
 
-extension StytchClient {
+#if !os(tvOS) && !os(watchOS)
+public extension StytchClient {
     /// The interface for interacting with biometrics products.
     static var biometrics: Biometrics { .init(router: router.scopedRouter(BaseRoute.biometrics)) }
 }
+#endif
 
-extension StytchClient.Biometrics {
+public extension StytchClient.Biometrics {
     struct AuthenticateParameters {
         let sessionDuration: Minutes
+
+        public init(sessionDuration: Minutes = .defaultSessionDuration) {
+            self.sessionDuration = sessionDuration
+        }
     }
 
+    struct RegisterParameters {
+        // email or phone number
+        public let identifier: String
+        public let accessPolicy: AccessPolicy
+        public let sessionDuration: Minutes
+
+        public init(
+            identifier: String,
+            accessPolicy: StytchClient.Biometrics.RegisterParameters.AccessPolicy = .deviceOwnerAuthenticationWithBiometrics,
+            sessionDuration: Minutes = .defaultSessionDuration
+        ) {
+            self.identifier = identifier
+            self.accessPolicy = accessPolicy
+            self.sessionDuration = sessionDuration
+        }
+    }
+}
+
+public extension StytchClient.Biometrics.RegisterParameters {
+    enum AccessPolicy {
+        case deviceOwnerAuthentication
+        case deviceOwnerAuthenticationWithBiometrics
+        #if os(macOS)
+        // swiftlint:disable:next identifier_name
+        case deviceOwnerAuthenticationWithBiometricsOrWatch
+        #endif
+
+        var keychainValue: KeychainClient.Item.AccessPolicy {
+            switch self {
+            case .deviceOwnerAuthentication:
+                return .deviceOwnerAuthentication
+            case .deviceOwnerAuthenticationWithBiometrics:
+                return .deviceOwnerAuthenticationWithBiometrics
+            #if os(macOS)
+            case .deviceOwnerAuthenticationWithBiometricsOrWatch:
+                return .deviceOwnerAuthenticationWithBiometricsOrWatch
+            #endif
+            }
+        }
+    }
+}
+
+extension StytchClient.Biometrics {
     struct AuthenticateStartParameters: Encodable {
         let publicKey: Data
-        let userId: String
     }
 
     struct AuthenticateStartResponse: Codable {
@@ -113,78 +156,36 @@ extension StytchClient.Biometrics {
         let biometricRegistrationId: String
     }
 
-    struct AuthenticateCompleteParameters: Encodable {
+    struct AuthenticateCompleteParameters: Codable {
         let signature: Data
         let biometricRegistrationId: String
         let sessionDurationMinutes: Minutes
-    }
-
-    struct Registration {
-        public let userLabel: String
-        public let createdAt: Date
-        let index: Int
-    }
-
-    struct RegisterParameters {
-        // email or phone number
-        let identifier: String
-        let accessPolicy: AccessPolicy
-        let syncingBehavior: SyncingBehavior
-
-        enum AccessPolicy {
-            case deviceOwnerAuthentication
-            case deviceOwnerAuthenticationWithBiometrics
-            #if os(macOS)
-            // swiftlint:disable:next identifier_name
-            case deviceOwnerAuthenticationWithBiometricsOrWatch
-            #endif
-
-            var keychainValue: KeychainClient.Item.AccessPolicy {
-                switch self {
-                case .deviceOwnerAuthentication:
-                    return .deviceOwnerAuthentication
-                case .deviceOwnerAuthenticationWithBiometrics:
-                    return .deviceOwnerAuthenticationWithBiometrics
-                #if os(macOS)
-                case .deviceOwnerAuthenticationWithBiometricsOrWatch:
-                    return .deviceOwnerAuthenticationWithBiometricsOrWatch
-                #endif
-                }
-            }
-        }
-
-        enum SyncingBehavior {
-            case disabled
-            case enabled
-
-            var keychainValue: KeychainClient.Item.SyncingBehavior {
-                switch self {
-                case .disabled:
-                    return .disabled
-                case .enabled:
-                    return .enabled
-                }
-            }
-        }
     }
 
     private struct RegisterStartParameters: Encodable {
         let publicKey: Data
     }
 
-    private struct RegisterStartResponse: Decodable {
+    struct RegisterStartResponse: Codable {
         let biometricRegistrationId: String
         let challenge: Data
     }
 
     private struct RegisterFinishParameters: Encodable {
+        private enum CodingKeys: String, CodingKey {
+            case biometricRegistrationId, signature, sessionDuration = "session_duration_minutes"
+        }
+
         let biometricRegistrationId: String
         let signature: Data
+        let sessionDuration: Minutes
     }
 
-    private struct RegisterCompleteResponseData: Codable {
+    struct RegisterCompleteResponseData: Codable, AuthenticateResponseType {
         let biometricRegistrationId: String
         let user: User
+        let session: Session
+        let sessionToken: String
+        let sessionJwt: String
     }
 }
-#endif
