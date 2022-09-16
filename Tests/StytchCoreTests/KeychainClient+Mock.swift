341e2eb1404c7fa4f32c5c33b4b824230a876178
Dan Loman <daniel.h.loman@gmail.com>
2022-09-16 15:00:47 -0700

    Improve KeychainClient mock

--- Tests/StytchCoreTests/KeychainClient+Mock.swift
+++ Tests/StytchCoreTests/KeychainClient+Mock.swift
@@ -7,9 +7,14 @@ extension KeychainClient {
         return .init { item in
             keychainItems[item.name] ?? []
         } setValueForItem: { value, item in
-            keychainItems[item.name] = [
-                .init(data: value.data, createdAt: .init(), modifiedAt: .init(), label: value.label, account: value.account ?? "not_set", generic: nil),
-            ]
+            let queryResult: KeychainClient.QueryResult = .init(data: value.data, createdAt: .init(), modifiedAt: .init(), label: value.label, account: value.account, generic: nil)
+            var results = keychainItems[item.name, default: []]
+            if let index = results.firstIndex(where: { $0.label == queryResult.label && $0.account == queryResult.account }) {
+                results[index] = queryResult
+            } else {
+                results.append(queryResult)
+            }
+            keychainItems[item.name] = results
         } removeItem: { item in
             keychainItems[item.name] = nil
         }
