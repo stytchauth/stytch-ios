656a792ec90d6b17323442742588d428bd3f552d
Dan Loman <daniel.h.loman@gmail.com>
2022-09-18 13:32:28 -0700

    Add documentation for biometrics types/methods

--- Sources/StytchCore/StytchClient/Biometrics/StytchClient+Biometrics.swift
+++ Sources/StytchCore/StytchClient/Biometrics/StytchClient+Biometrics.swift
@@ -1,23 +1,25 @@
 import Foundation
 
 public extension StytchClient {
-    /// some docs
+    /// Biometric authentication enables your users to leverage their devices' built-in biometric authenticators such as FaceID and TouchID for quick and seamless login experiences.
     struct Biometrics {
         let router: NetworkingRouter<BiometricsRoute>
 
-        private static let biometricRegistrationKey: String = "biometric_registration_available"
-
+        /// Indicates if there is an existing biometric registration on device.
         public var registrationAvailable: Bool {
-            Current.defaults.bool(forKey: Self.biometricRegistrationKey)
+            Current.defaults.bool(forKey: Self.biometricRegistrationDefaultsKey)
         }
 
+        /// Clears existing biometric registrations stored on device. Useful when removing a user from a given device.
         public func removeRegistration() throws {
             try Current.keychainClient.removeItem(.privateKeyRegistration)
-            Current.defaults.removeObject(forKey: Self.biometricRegistrationKey)
+            Current.defaults.removeObject(forKey: Self.biometricRegistrationDefaultsKey)
         }
 
-        // sourcery: AsyncAsyncVariants, (NOTE: - must use /// doc comment styling)
-        /// some docs
+        // sourcery: AsyncVariants, (NOTE: - must use /// doc comment styling)
+        /// When a valid/active session exists, this method will add a biometric registration for the current user. The user will later be able to start a new session with biometrics or use biometrics as an additional authentication factor.
+        ///
+        /// NOTE: - You should ensure the `accessPolicy` parameters match your particular needs, defaults to `deviceOwnerWithBiometrics`.
         public func register(parameters: RegisterParameters) async throws -> AuthenticateResponseType {
             // Early out if not authenticated
             guard (Current.sessionStorage.sessionToken ?? Current.sessionStorage.sessionJwt) != nil else {
@@ -55,13 +57,13 @@ public extension StytchClient {
                 accessPolicy: parameters.accessPolicy.keychainValue
             )
 
-            Current.defaults.set(true, forKey: Self.biometricRegistrationKey)
+            Current.defaults.set(true, forKey: Self.biometricRegistrationDefaultsKey)
 
             return finishResponse
         }
 
-        // sourcery: AsyncAsyncVariants, (NOTE: - must use /// doc comment styling)
-        /// some docs
+        // sourcery: AsyncVariants, (NOTE: - must use /// doc comment styling)
+        /// If a valid biometric registration exists, this method confirms the current device owner via the device's built-in biometric reader and returns an updated session object by either starting a new session or adding a the biometric factor to an existing session.
         public func authenticate(parameters: AuthenticateParameters) async throws -> AuthenticateResponseType {
             guard let queryResult: KeychainClient.QueryResult = try Current.keychainClient.get(.privateKeyRegistration).first else {
                 throw StytchError.noBiometricRegistrationsAvailable
@@ -75,7 +77,7 @@ public extension StytchClient {
                 parameters: AuthenticateStartParameters(publicKey: publicKey)
             )
 
-            // NOTE: - Could return separate concrete type which deserializes/contains biometric_registration_id, but this doesn't seem to add much value
+            // NOTE: - We could return separate concrete type which deserializes/contains biometric_registration_id, but this doesn't currently add much value
             return try await router.post(
                 to: .authenticate(.complete),
                 parameters: AuthenticateCompleteParameters(
@@ -96,20 +98,27 @@ public extension StytchClient {
 #endif
 
 public extension StytchClient.Biometrics {
+    /// A dedicated parameters type for biometrics `authenticate` calls.
     struct AuthenticateParameters {
         let sessionDuration: Minutes
 
+        /// Initializes the parameters struct
+        /// - Parameter sessionDuration: The duration, in minutes, for the requested session. Defaults to 30 minutes.
         public init(sessionDuration: Minutes = .defaultSessionDuration) {
             self.sessionDuration = sessionDuration
         }
     }
-
+    /// A dedicated parameters type for biometrics `register` calls.
     struct RegisterParameters {
-        // email or phone number
-        public let identifier: String
-        public let accessPolicy: AccessPolicy
-        public let sessionDuration: Minutes
+        let identifier: String
+        let accessPolicy: AccessPolicy
+        let sessionDuration: Minutes
 
+        /// Initializes the parameters struct
+        /// - Parameters:
+        ///   - identifier: An id used to easily identify the account associated with the biometric registration, generally an email or phone number.
+        ///   - accessPolicy: Defines the policy as to how the user must confirm their ownership.
+        ///   - sessionDuration: The duration, in minutes, for the requested session. Defaults to 30 minutes.
         public init(
             identifier: String,
             accessPolicy: StytchClient.Biometrics.RegisterParameters.AccessPolicy = .deviceOwnerAuthenticationWithBiometrics,
@@ -123,11 +132,15 @@ public extension StytchClient.Biometrics {
 }
 
 public extension StytchClient.Biometrics.RegisterParameters {
+    /// Defines the policy as to how the user must confirm their device ownership.
     enum AccessPolicy {
+        /// The device will first try to confirm access rights via biometrics and will fall back to device passcode.
         case deviceOwnerAuthentication
+        /// The device will try to confirm access rights via biometrics.
         case deviceOwnerAuthenticationWithBiometrics
         #if os(macOS)
         // swiftlint:disable:next identifier_name
+        /// The device will, in parallel, try to confirm access rights via biometrics or an associated Apple Watch.
         case deviceOwnerAuthenticationWithBiometricsOrWatch
         #endif
 
@@ -146,7 +159,10 @@ public extension StytchClient.Biometrics.RegisterParameters {
     }
 }
 
+// Internal/private parameters and keys
 extension StytchClient.Biometrics {
+    private static let biometricRegistrationDefaultsKey: String = "biometric_registration_available"
+
     struct AuthenticateStartParameters: Encodable {
         let publicKey: Data
     }
