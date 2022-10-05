cdb85d8e84ad5fa3a5e6834ff13f540779618daf
Dan Loman <daniel.h.loman@gmail.com>
2022-10-05 15:39:56 -0700

    Add third-party provider docs

--- Sources/StytchCore/StytchClient/OAuth/OAuth+ThirdPartyProvider.swift
+++ Sources/StytchCore/StytchClient/OAuth/OAuth+ThirdPartyProvider.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 public extension StytchClient.OAuth {
-    /// docs
+    /// The SDK provides the ability to integrate with third-party identity providers for OAuth experiences beyond the natively supported Sign In With Apple flow.
     struct ThirdPartyProvider {
         let provider: Provider
 
-        /// docs
+        /// Initiates the OAuth flow by using the included parameters to generate a URL and pass this off to the system's default browser. The user will be redirected to the corresponding redirectUrl (this should be back into the application), after completing the authentication challenge with the identity provider.
         public func start(parameters: StartParameters) throws {
             guard let publicToken = StytchClient.instance.configuration?.publicToken else { throw StytchError.clientNotConfigured }
 
@@ -32,13 +32,16 @@ public extension StytchClient.OAuth {
             Current.openUrl(url)
         }
 
-        /// docs
+        /// The dedicated parameters type for the ``start(parameters:)`` call.
         public struct StartParameters: Encodable {
             let loginRedirectUrl: URL?
             let signupRedirectUrl: URL?
             let customScopes: [String]?
 
-            /// docs
+            /// - Parameters:
+            ///   - loginRedirectUrl: The url an existing user is redirected to after authenticating with the identity provider. This should be a url that redirects back to your app. If this value is not passed, the default login redirect URL set in the Stytch Dashboard is used. If you have not set a default login redirect URL, an error is returned.
+            ///   - signupRedirectUrl: The url a new user is redirected to after authenticating with the identity provider. This should be a url that redirects back to your app. If this value is not passed, the default sign-up redirect URL set in the Stytch Dashboard is used. If you have not set a default sign-up redirect URL, an error is returned.
+            ///   - customScopes: Any additional scopes to be requested from the identity provider.
             public init(
                 loginRedirectUrl: URL? = nil,
                 signupRedirectUrl: URL? = nil,
