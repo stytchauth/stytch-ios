8fb852eb247c6bf15cc3846194a2413d00ea8551
Dan Loman <daniel.h.loman@gmail.com>
2022-10-14 13:52:53 -0700

    Add codegen support for availability attributes

---
+++ Sources/StytchCore/Generated/StytchClient.Passkeys.authenticate+AsyncVariants.generated.swift
@@ -0,0 +1,32 @@
+// Generated using Sourcery 1.8.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+import Combine
+import Foundation
+
+@available(macOS 12.0, iOS 16.0, tvOS 16.0, *)
+public extension StytchClient.Passkeys {
+    func authenticate(parameters: AuthenticateParameters, completion: @escaping Completion<AuthenticateResponseType>) {
+        Task {
+            do {
+                completion(.success(try await authenticate(parameters: parameters)))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func authenticate(parameters: AuthenticateParameters) -> AnyPublisher<AuthenticateResponseType, Error> {
+        return Deferred {
+            Future({ promise in
+                Task {
+                    do {
+                        promise(.success(try await authenticate(parameters: parameters)))
+                    } catch {
+                        promise(.failure(error))
+                    }
+                }
+            })
+        }
+        .eraseToAnyPublisher()
+    }
+}
---
+++ Sources/StytchCore/Generated/StytchClient.Passkeys.register+AsyncVariants.generated.swift
@@ -0,0 +1,32 @@
+// Generated using Sourcery 1.8.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+import Combine
+import Foundation
+
+@available(macOS 12.0, iOS 16.0, tvOS 16.0, *)
+public extension StytchClient.Passkeys {
+    func register(parameters: RegisterParameters, completion: @escaping Completion<BasicResponse>) {
+        Task {
+            do {
+                completion(.success(try await register(parameters: parameters)))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func register(parameters: RegisterParameters) -> AnyPublisher<BasicResponse, Error> {
+        return Deferred {
+            Future({ promise in
+                Task {
+                    do {
+                        promise(.success(try await register(parameters: parameters)))
+                    } catch {
+                        promise(.failure(error))
+                    }
+                }
+            })
+        }
+        .eraseToAnyPublisher()
+    }
+}
