b151ff87dbc759da94d3e37d9efdd5dcabd1fc21
Dan Loman <dan@stytch.com>
2022-12-07 14:34:29 -0800

    Enable biometric registration deletion

--- Sources/StytchCore/StytchClient/User/StytchClient+User.swift
+++ Sources/StytchCore/StytchClient/User/StytchClient+User.swift
@@ -1,5 +1,5 @@
 public extension StytchClient {
-    /// The SDK allows you to manage the current user's information, such as fetching the user, view the most-recent cached version of the user, or deleting existing authentication factors associated with this user.
+    /// The SDK allows you to manage the current user's information, such as fetching the user, view the most recent cached version of the user, or deleting existing authentication factors associated with this user.
     struct UserManagement {
         let router: NetworkingRouter<UsersRoute>
 
@@ -21,14 +21,16 @@ public extension StytchClient {
         public func deleteFactor(_ factor: AuthenticationFactor) async throws -> UserResponse {
             try await updatingCachedUser {
                 switch factor {
+                case let .biometricRegistration(id):
+                    return try await router.delete(route: .factors(.biometricRegistrations(id: id)))
+                case let .cryptoWallet(id):
+                    return try await router.delete(route: .factors(.cryptoWallets(id: id)))
                 case let .email(id):
                     return try await router.delete(route: .factors(.emails(id: id)))
                 case let .phoneNumber(id):
                     return try await router.delete(route: .factors(.phoneNumbers(id: id)))
                 case let .webAuthnRegistration(id):
                     return try await router.delete(route: .factors(.webAuthNRegistrations(id: id)))
-                case let .cryptoWallet(id):
-                    return try await router.delete(route: .factors(.cryptoWallets(id: id)))
                 }
             }
         }
@@ -52,9 +54,10 @@ public typealias UserResponse = Response<User>
 public extension StytchClient.UserManagement {
     /// The authentication factors which are able to be managed via user-management calls.
     enum AuthenticationFactor {
+        case biometricRegistration(id: User.BiometricRegistration.ID)
+        case cryptoWallet(id: User.CryptoWallet.ID)
         case email(id: User.Email.ID)
         case phoneNumber(id: User.PhoneNumber.ID)
         case webAuthnRegistration(id: User.WebAuthNRegistration.ID)
-        case cryptoWallet(id: User.CryptoWallet.ID)
     }
 }
--- Sources/StytchCore/StytchClient/User/UsersRoute.swift
+++ Sources/StytchCore/StytchClient/User/UsersRoute.swift
@@ -14,22 +14,25 @@ enum UsersRoute: RouteType {
 
 extension UsersRoute {
     enum FactorsRoute: RouteType {
+        case biometricRegistrations(id: User.BiometricRegistration.ID)
+        case cryptoWallets(id: User.CryptoWallet.ID)
         case emails(id: User.Email.ID)
         case phoneNumbers(id: User.PhoneNumber.ID)
         case webAuthNRegistrations(id: User.WebAuthNRegistration.ID)
-        case cryptoWallets(id: User.CryptoWallet.ID)
 
         var path: Path {
             let joinedPath: (Path, String) -> Path = { $0.appendingPath(.init(rawValue: $1)) }
             switch self {
+            case let .biometricRegistrations(id):
+                return joinedPath("biometric_registrations", id.rawValue)
+            case let .cryptoWallets(id):
+                return joinedPath("crypto_wallets", id.rawValue)
             case let .emails(id):
                 return joinedPath("emails", id.rawValue)
             case let .phoneNumbers(id):
                 return joinedPath("phone_numbers", id.rawValue)
             case let .webAuthNRegistrations(id):
                 return joinedPath("webauthn_registrations", id.rawValue)
-            case let .cryptoWallets(id):
-                return joinedPath("crypto_wallets", id.rawValue)
             }
         }
     }
