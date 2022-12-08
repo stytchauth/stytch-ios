c0d9bf8bc4066258f19dc0ddfc0908d245c96b2f
Dan Loman <dan@stytch.com>
2022-12-08 13:58:42 -0800

    Add PKCE to EML send

--- Sources/StytchCore/StytchClient/MagicLinks/Email/Email.swift
+++ Sources/StytchCore/StytchClient/MagicLinks/Email/Email.swift
@@ -23,9 +23,11 @@ public extension StytchClient.MagicLinks {
         // sourcery: AsyncVariants, (NOTE: - must use /// doc comment styling)
         /// Wraps Stytch's email magic link [send](https://stytch.com/docs/api/send-by-email) endpoint. Requests an email magic link for an existing user to log in or attach the included email factor to their current account.
         public func send(parameters: SendParameters) async throws -> BasicResponse {
-            try await router.post(
+            let (codeChallenge, codeChallengeMethod) = try StytchClient.generateAndStorePKCE(keychainItem: .emlPKCECodeVerifier)
+
+            return try await router.post(
                 to: Current.sessionStorage.activeSessionExists ? .sendSecondary : .sendPrimary,
-                parameters: parameters
+                parameters: CodeChallengedParameters(codeChallenge: codeChallenge, codeChallengeMethod: codeChallengeMethod, wrapped: parameters)
             )
         }
     }
