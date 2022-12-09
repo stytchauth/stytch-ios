3d2febb86d09b56b35fd87055117194388c3f1aa
Dan Loman <dan@stytch.com>
2022-12-08 18:32:42 -0800

    Make XCTest helpers extensions on XCTest

--- Tests/StytchCoreTests/BaseTestCase.swift
+++ Tests/StytchCoreTests/BaseTestCase.swift
@@ -36,23 +36,6 @@ class BaseTestCase: XCTestCase {
     }
 }
 
-extension XCTest {
-    func XCTAssertThrowsError<T: Sendable>(
-        _ expression: @autoclosure () async throws -> T,
-        _ message: @autoclosure () -> String = "",
-        file: StaticString = #filePath,
-        line: UInt = #line,
-        _ errorHandler: (_ error: Error) -> Void = { _ in }
-    ) async {
-        do {
-            _ = try await expression()
-            XCTFail(message(), file: file, line: line)
-        } catch {
-            errorHandler(error)
-        }
-    }
-}
-
 extension Sequence {
     func asyncForEach(_ operation: (Element) async throws -> Void) async rethrows {
         for element in self {
