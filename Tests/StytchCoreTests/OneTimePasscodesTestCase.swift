40b506d6de88aba1e07971c940bd7130b5c83760
Dan Loman <dan@stytch.com>
2023-01-10 14:58:21 -0800

    Migrate several more testcases to use responses builder and network interceptor

--- Tests/StytchCoreTests/OneTimePasscodesTestCase.swift
+++ Tests/StytchCoreTests/OneTimePasscodesTestCase.swift
@@ -3,38 +3,39 @@ import XCTest
 
 final class OneTimePasscodesTestCase: BaseTestCase {
     func testOtpLoginOrCreate() async throws {
-        let container: DataContainer<StytchClient.OneTimePasscodes.OTPResponse> = .init(
-            data: .init(
-                requestId: "1234",
-                statusCode: 200,
-                wrapped: .init(methodId: "method_id_1234")
-            )
+        let response: StytchClient.OneTimePasscodes.OTPResponse = .init(
+            requestId: "1234",
+            statusCode: 200,
+            wrapped: .init(methodId: "method_id_1234")
         )
-        let data = try Current.jsonEncoder.encode(container)
-        var request: URLRequest?
-        Current.networkingClient = .mock(verifyingRequest: { request = $0 }, returning: .success(data), .success(data), .success(data))
+        networkInterceptor.responses {
+            response
+            response
+            response
+        }
 
         try await [
-            (
-                StytchClient.OneTimePasscodes.Parameters(deliveryMethod: .whatsapp(phoneNumber: "+12345678901"), expiration: 3),
-                "https://web.stytch.com/sdk/v1/otps/whatsapp/login_or_create",
-                JSON.object(["expiration_minutes": 3, "phone_number": "+12345678901"])
+            ExpectedValues(
+                parameters: StytchClient.OneTimePasscodes.Parameters(deliveryMethod: .whatsapp(phoneNumber: "+12345678901"), expiration: 3),
+                urlString: "https://web.stytch.com/sdk/v1/otps/whatsapp/login_or_create",
+                body: ["expiration_minutes": 3, "phone_number": "+12345678901"]
             ),
-            (
-                .init(deliveryMethod: .sms(phoneNumber: "+11098765432")),
-                "https://web.stytch.com/sdk/v1/otps/sms/login_or_create",
-                ["phone_number": "+11098765432"]
+            .init(
+                parameters: .init(deliveryMethod: .sms(phoneNumber: "+11098765432")),
+                urlString: "https://web.stytch.com/sdk/v1/otps/sms/login_or_create",
+                body: ["phone_number": "+11098765432"]
             ),
-            (
-                .init(deliveryMethod: .email("test@stytch.com")),
-                "https://web.stytch.com/sdk/v1/otps/email/login_or_create",
-                ["email": "test@stytch.com"]
+            .init(
+                parameters: .init(deliveryMethod: .email("test@stytch.com")),
+                urlString: "https://web.stytch.com/sdk/v1/otps/email/login_or_create",
+                body: ["email": "test@stytch.com"]
             ),
         ]
-        .asyncForEach { params, urlString, body in
-            _ = try await StytchClient.otps.loginOrCreate(parameters: params)
+        .enumerated()
+        .asyncForEach { index, expected in
+            _ = try await StytchClient.otps.loginOrCreate(parameters: expected.parameters)
 
-            try XCTAssertRequest(request, urlString: urlString, method: .post(body))
+            try XCTAssertRequest(networkInterceptor.requests[index], urlString: expected.urlString, method: .post(expected.body))
 
             XCTAssertNil(StytchClient.sessions.sessionJwt)
             XCTAssertNil(StytchClient.sessions.sessionToken)
@@ -42,21 +43,21 @@ final class OneTimePasscodesTestCase: BaseTestCase {
     }
 
     func testOtpSendWithNoSession() async throws {
-        let container: DataContainer<StytchClient.OneTimePasscodes.OTPResponse> = .init(
-            data: .init(
-                requestId: "1234",
-                statusCode: 200,
-                wrapped: .init(methodId: "method_id_1234")
-            )
+        let response: StytchClient.OneTimePasscodes.OTPResponse = .init(
+            requestId: "1234",
+            statusCode: 200,
+            wrapped: .init(methodId: "method_id_1234")
         )
-        let data = try Current.jsonEncoder.encode(container)
-        var request: URLRequest?
-        Current.networkingClient = .mock(verifyingRequest: { request = $0 }, returning: .success(data), .success(data), .success(data))
+        networkInterceptor.responses {
+            response
+            response
+            response
+        }
 
         XCTAssertFalse(Current.sessionStorage.activeSessionExists)
 
-        let expectedValues: [ExpectedValues] = [
-            .init(
+        try await [
+            ExpectedValues(
                 parameters: .init(deliveryMethod: .whatsapp(phoneNumber: "+12345678901"), expiration: 3),
                 urlString: "https://web.stytch.com/sdk/v1/otps/whatsapp/send/primary",
                 body: ["expiration_minutes": 3, "phone_number": "+12345678901"]
@@ -72,10 +73,11 @@ final class OneTimePasscodesTestCase: BaseTestCase {
                 body: ["email": "test@stytch.com"]
             ),
         ]
-        try await expectedValues.asyncForEach { expected in
+        .enumerated()
+        .asyncForEach { index, expected in
             _ = try await StytchClient.otps.send(parameters: expected.parameters)
 
-            try XCTAssertRequest(request, urlString: expected.urlString, method: .post(expected.body))
+            try XCTAssertRequest(networkInterceptor.requests[index], urlString: expected.urlString, method: .post(expected.body))
 
             XCTAssertNil(StytchClient.sessions.sessionJwt)
             XCTAssertNil(StytchClient.sessions.sessionToken)
@@ -83,23 +85,23 @@ final class OneTimePasscodesTestCase: BaseTestCase {
     }
 
     func testOtpSendWithActiveSession() async throws {
-        let container: DataContainer<StytchClient.OneTimePasscodes.OTPResponse> = .init(
-            data: .init(
-                requestId: "1234",
-                statusCode: 200,
-                wrapped: .init(methodId: "method_id_1234")
-            )
+        let response: StytchClient.OneTimePasscodes.OTPResponse = .init(
+            requestId: "1234",
+            statusCode: 200,
+            wrapped: .init(methodId: "method_id_1234")
         )
-        let data = try Current.jsonEncoder.encode(container)
-        var request: URLRequest?
-        Current.networkingClient = .mock(verifyingRequest: { request = $0 }, returning: .success(data), .success(data), .success(data))
+        networkInterceptor.responses {
+            response
+            response
+            response
+        }
 
         try Current.keychainClient.set("123", for: .sessionToken)
 
         XCTAssertTrue(Current.sessionStorage.activeSessionExists)
 
-        let expectedValues: [ExpectedValues] = [
-            .init(
+        try await [
+            ExpectedValues(
                 parameters: .init(deliveryMethod: .whatsapp(phoneNumber: "+12345678901"), expiration: 3),
                 urlString: "https://web.stytch.com/sdk/v1/otps/whatsapp/send/secondary",
                 body: ["expiration_minutes": 3, "phone_number": "+12345678901"]
@@ -115,18 +117,15 @@ final class OneTimePasscodesTestCase: BaseTestCase {
                 body: ["email": "test@stytch.com"]
             ),
         ]
-        try await expectedValues.asyncForEach { expected in
+        .enumerated()
+        .asyncForEach { index, expected in
             _ = try await StytchClient.otps.send(parameters: expected.parameters)
-            try XCTAssertRequest(request, urlString: expected.urlString, method: .post(expected.body))
+            try XCTAssertRequest(networkInterceptor.requests[index], urlString: expected.urlString, method: .post(expected.body))
         }
     }
 
     func testOtpAuthenticate() async throws {
-        let authResponse: AuthenticateResponse = .mock
-        let container: DataContainer<AuthenticateResponse> = .init(data: authResponse)
-        let data = try Current.jsonEncoder.encode(container)
-        var request: URLRequest?
-        Current.networkingClient = .mock(verifyingRequest: { request = $0 }, returning: .success(data))
+        networkInterceptor.responses { AuthenticateResponse.mock }
         let parameters: StytchClient.OneTimePasscodes.AuthenticateParameters = .init(
             code: "i_am_code",
             methodId: "method_id_fake_id",
@@ -144,7 +143,7 @@ final class OneTimePasscodesTestCase: BaseTestCase {
         XCTAssertEqual(StytchClient.sessions.sessionJwt, .jwt("jwt_for_me"))
 
         try XCTAssertRequest(
-            request,
+            networkInterceptor.requests[0],
             urlString: "https://web.stytch.com/sdk/v1/otps/authenticate",
             method: .post([
                 "token": "i_am_code",
