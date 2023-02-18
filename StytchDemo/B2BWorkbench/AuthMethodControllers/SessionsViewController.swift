80d8d2b2cce8a44ff75f2a262cc2c3f2421c3858
Dan Loman <dan@stytch.com>
2023-02-17 18:40:50 -0800

    Add B2B workbench app

---
+++ StytchDemo/B2BWorkbench/AuthMethodControllers/SessionsViewController.swift
@@ -0,0 +1,66 @@
+import StytchCore
+import UIKit
+
+final class SessionsViewController: UIViewController {
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.layoutMargins = Constants.insets
+        view.isLayoutMarginsRelativeArrangement = true
+        view.axis = .vertical
+        view.spacing = 8
+        return view
+    }()
+
+    private lazy var authenticateButton: UIButton = {
+        var configuration: UIButton.Configuration = .borderedProminent()
+        configuration.title = "Authenticate"
+        return .init(configuration: configuration, primaryAction: authenticateAction)
+    }()
+
+    private lazy var revokeButton: UIButton = {
+        var configuration: UIButton.Configuration = .borderedProminent()
+        configuration.title = "Revoke"
+        return .init(configuration: configuration, primaryAction: revokeAction)
+    }()
+
+    private lazy var authenticateAction: UIAction = .init { _ in
+        Task {
+            do {
+                let resp = try await StytchB2BClient.sessions.authenticate(parameters: .init())
+                print(resp)
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    private lazy var revokeAction: UIAction = .init { _ in
+        Task {
+            do {
+                let resp = try await StytchB2BClient.sessions.revoke()
+                print(resp)
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Sessions"
+
+        view.backgroundColor = .systemBackground
+
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+
+        stackView.addArrangedSubview(authenticateButton)
+        stackView.addArrangedSubview(revokeButton)
+    }
+}
--- StytchDemo/B2BWorkbench/SceneDelegate.swift
+++ StytchDemo/B2BWorkbench/SceneDelegate.swift
@@ -1,52 +1,35 @@
-//
-//  SceneDelegate.swift
-//  B2BWorkbench
-//
-//  Created by Dan Loman on 2/16/23.
-//
-
+import StytchCore
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    func scene(_: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else { return }
+
+        handle(url: url)
     }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    private func handle(url: URL) {
+        Task {
+            do {
+                switch try await StytchB2BClient.handle(url: url, sessionDuration: 60) {
+                case let .handled(response):
+                    // Handled via RootVC onAuthChange publisher
+                    print(response)
+                case .manualHandlingRequired, .notHandled:
+                    break
+                }
+            } catch {
+                print(error)
+            }
+        }
     }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
-
