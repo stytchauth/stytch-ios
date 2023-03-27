4212bf2cc6fce16ed092838445f25433587e5ea7
Dan Loman <dan@stytch.com>
2023-03-27 16:46:36 -0700

    SendTokensInAuthHeader

---
+++ Sources/StytchCore/Documentation.docc/Resources/Code/SendTokensInAuthHeader/01-send-tokens-in-auth-header-04.swift
@@ -0,0 +1,15 @@
+import StytchCore
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    let networkingClient: NetworkingClient = .init()
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        StytchClient.configure(publicToken: publicToken)
+        return true
+    }
+}
