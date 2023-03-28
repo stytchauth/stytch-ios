d8e30d23adc929e9839d7954333f502db6a0408a
Dan Loman <dan@stytch.com>
2023-03-27 20:11:30 -0700

    Add to ManageTheCurrentUser

---
+++ Sources/StytchCore/Documentation.docc/Resources/Code/ManageTheCurrentUser/03-manage-the-current-user-03.swift
@@ -0,0 +1,13 @@
+import StytchCore
+import UIKit
+
+final class UserSettingsViewController: UIViewController {
+    private func attachToUser(email: String) {
+        Task {
+            do {
+                _ = try await StytchClient.magicLinks.email.send(parameters: .init(email: userEmail))
+                showAlertToCheckEmail()
+            } catch {}
+        }
+    }
+}
