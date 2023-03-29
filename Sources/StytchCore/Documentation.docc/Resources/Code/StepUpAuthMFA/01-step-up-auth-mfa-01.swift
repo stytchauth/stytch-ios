e358bb75e7df829b080fa37eb6114c8d12b527eb
Dan Loman <dan@stytch.com>
2023-03-28 19:01:48 -0700

    StepUpAuthMFA

---
+++ Sources/StytchCore/Documentation.docc/Resources/Code/StepUpAuthMFA/01-step-up-auth-mfa-01.swift
@@ -0,0 +1,12 @@
+import StytchCore
+import UIKit
+
+final class UserSettingsViewController: UIViewController {
+    private let editSettingsButton: UIButton
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ...
+    }
+}
