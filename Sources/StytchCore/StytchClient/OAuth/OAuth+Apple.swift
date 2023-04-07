f0761eeb75d6ee1b236df9bbe1a2b2c98e2c7528
Dan Loman <dan@stytch.com>
2023-04-07 16:30:03 -0700

    Lint

--- Sources/StytchCore/StytchClient/OAuth/OAuth+Apple.swift
+++ Sources/StytchCore/StytchClient/OAuth/OAuth+Apple.swift
@@ -35,6 +35,23 @@ public extension StytchClient.OAuth {
 }
 
 public extension StytchClient.OAuth.Apple {
+    /// The concrete response type for Sign In With Apple `authenticate` calls.
+    typealias AuthenticateResponse = Response<AuthenticateResponseData>
+
+    /// The underlying data for Sign In With Apple `authenticate` calls.
+    struct AuthenticateResponseData: Codable, AuthenticateResponseDataType {
+        /// The current user object.
+        public let user: User
+        /// The opaque token for the session. Can be used by your server to verify the validity of your session by confirming with Stytch's servers on each request.
+        public let sessionToken: String
+        /// The JWT for the session. Can be used by your server to verify the validity of your session either by checking the data included in the JWT, or by verifying with Stytch's servers as needed.
+        public let sessionJwt: String
+        /// The ``Session`` object, which includes information about the session's validity, expiry, factors associated with this session, and more.
+        public let session: Session
+        /// Indicates if this is a new or returning user
+        public let userCreated: Bool
+    }
+
     /// The dedicated parameters type for ``StytchClient/OAuth-swift.struct/Apple-swift.struct/start(parameters:)-7rkef`` calls.
     struct StartParameters {
         let sessionDuration: Minutes
@@ -60,23 +77,6 @@ public extension StytchClient.OAuth.Apple {
             self.sessionDuration = sessionDuration
         }
         #endif
-    }
-
-    /// The concrete response type for Sign In With Apple `authenticate` calls.
-    typealias AuthenticateResponse = Response<AuthenticateResponseData>
-
-    /// The underlying data for Sign In With Apple `authenticate` calls.
-    struct AuthenticateResponseData: Codable, AuthenticateResponseDataType {
-        /// The current user object.
-        public let user: User
-        /// The opaque token for the session. Can be used by your server to verify the validity of your session by confirming with Stytch's servers on each request.
-        public let sessionToken: String
-        /// The JWT for the session. Can be used by your server to verify the validity of your session either by checking the data included in the JWT, or by verifying with Stytch's servers as needed.
-        public let sessionJwt: String
-        /// The ``Session`` object, which includes information about the session's validity, expiry, factors associated with this session, and more.
-        public let session: Session
-        /// Indicates if this is a new or returning user
-        public let userCreated: Bool
     }
 }
 
