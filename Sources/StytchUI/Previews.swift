cfe3ebf66b36b6a14b472c1dfde29dd6af34d611
Dan Loman <dan@stytch.com>
2023-05-21 13:50:05 -0700

    Add SwiftUI previews

---
+++ Sources/StytchUI/Previews.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+
+struct StytchUI_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthRootViewController(
+            config: .init(
+                oauth: .init(providers: [.thirdParty(.google), .apple]),
+                input: .magicLink(sms: true)
+            )
+        )
+        .toControllerView()
+        .previewDisplayName("Root")
+
+
+        ActionableInformationViewController(
+            state: .loaded(
+                .forgotPassword(email: "dan@stytch.com")
+            )
+        ) { .oauth($0) }
+            .inNavigationController()
+            .toControllerView()
+            .previewDisplayName("AIVC")
+
+        if #available(iOS 15, *) {
+            PasswordViewController(
+                state: .loaded(.init(
+                    email: "dan.loman@gmail.com",
+                    intent: .signup,
+                    magicLinksEnabled: true
+                ))
+            ) { _ in .oauth(.didTap(provider: .apple)) }
+            //            state: .loaded(
+            //                .forgotPassword(email: "dan@stytch.com")
+            //            )
+            //        ) { .oauth($0) }
+                .inNavigationController()
+                .toControllerView()
+                .previewDisplayName("PW")
+        }
+    }
+}
+
+struct ControllerView: UIViewControllerRepresentable {
+    private let controller: UIViewController
+
+    init(_ controller: UIViewController) {
+        self.controller = controller
+    }
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        controller
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+extension UIViewController {
+    func toControllerView() -> ControllerView {
+        .init(self)
+    }
+
+    func inNavigationController() -> UIViewController {
+        UINavigationController(rootViewController: self)
+    }
+}
