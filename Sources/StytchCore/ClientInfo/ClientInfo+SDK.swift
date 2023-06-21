9fee86e4e1a312fc31725e01b3dadcaa99457a57
Jordan Haven <117691317+jhaven-stytch@users.noreply.github.com>
2023-06-21 13:51:55 -0400

    Update stytch-swift to stytch-ios (#138)

--- Sources/StytchCore/ClientInfo/ClientInfo+SDK.swift
+++ Sources/StytchCore/ClientInfo/ClientInfo+SDK.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension ClientInfo {
     struct SDK: Encodable {
-        let identifier: String = "stytch-swift"
+        let identifier: String = "stytch-ios"
         let version: Version = .current
     }
 }
