c0eed9cba53d7321ff0dba33be970164c3f95fcf
Jordan Haven <jhaven@stytch.com>
2023-09-21 18:28:01 -0400

    why not

--- Package.swift
+++ Package.swift
@@ -6,19 +6,19 @@ let package = Package(
     name: "Stytch",
     platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        .library(name: "StytchCore", targets: ["StytchCore", "Recaptcha"]),
+        .library(name: "StytchCore", targets: ["StytchCore", "RecaptchaEnterprise"]),
     ],
     targets: [
         .target(
             name: "StytchCore",
             dependencies: [
-                .target(name: "Recaptcha", condition: .when(platforms: [.iOS])),
+                .target(name: "RecaptchaEnterprise", condition: .when(platforms: [.iOS])),
             ],
             resources: [
                 .copy("DFPClient/dfp.html"),
             ]
         ),
-        .binaryTarget(name: "Recaptcha", path: "libs/recaptcha-xcframework.xcframework.zip"),
+        .binaryTarget(name: "RecaptchaEnterprise", path: "libs/recaptcha-xcframework.xcframework.zip"),
         .testTarget(name: "StytchCoreTests", dependencies: ["StytchCore"]),
     ]
 )
