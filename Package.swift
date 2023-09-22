a819398bb7154bb85a937dd3eee863c5d73818f0
Jordan Haven <jhaven@stytch.com>
2023-09-22 14:01:11 -0400

    reverting package changes since they didn't change anything

--- Package.swift
+++ Package.swift
@@ -6,19 +6,21 @@ let package = Package(
     name: "Stytch",
     platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        .library(name: "StytchCore", targets: ["StytchCore", "RecaptchaEnterprise"]),
+        .library(name: "StytchCore", targets: ["StytchCore"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/GoogleCloudPlatform/recaptcha-enterprise-mobile-sdk", from: "18.3.0"),
     ],
     targets: [
         .target(
             name: "StytchCore",
             dependencies: [
-                .target(name: "RecaptchaEnterprise", condition: .when(platforms: [.iOS])),
+                .product(name: "RecaptchaEnterprise", package: "recaptcha-enterprise-mobile-sdk", condition: .when(platforms: [.iOS])),
             ],
             resources: [
                 .copy("DFPClient/dfp.html"),
             ]
         ),
-        .binaryTarget(name: "RecaptchaEnterprise", path: "libs/recaptcha-xcframework.xcframework.zip"),
         .testTarget(name: "StytchCoreTests", dependencies: ["StytchCore"]),
     ]
 )
