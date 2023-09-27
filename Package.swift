be45c5bb4ef73e862d86e8d610783a7226d13d10
Jordan Haven <jhaven@stytch.com>
2023-09-27 11:02:46 -0400

    Non dynamic

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Stytch",
     platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        .library(name: "StytchCore", type: .dynamic, targets: ["StytchCore"]),
+        .library(name: "StytchCore", targets: ["StytchCore"]),
     ],
     dependencies: [
         .package(url: "https://github.com/GoogleCloudPlatform/recaptcha-enterprise-mobile-sdk", from: "18.3.0"),
