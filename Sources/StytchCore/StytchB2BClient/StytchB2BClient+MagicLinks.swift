2e2d4f9227f8d941cc2324e42b2f3d822ed74ed3
Nidal Fakhouri <nfakhouri@stytch.com>
2024-05-20 16:01:19 -0400

    Add missing fields to InviteParameters

--- Sources/StytchCore/StytchB2BClient/StytchB2BClient+MagicLinks.swift
+++ Sources/StytchCore/StytchB2BClient/StytchB2BClient+MagicLinks.swift
@@ -211,29 +211,44 @@ public extension StytchB2BClient.MagicLinks.Email {
             case email = "emailAddress"
             case inviteRedirectUrl
             case inviteTemplateId
+            case name
+            case untrustedMetadata
             case locale
+            case roles
         }
 
         let email: String
         let inviteRedirectUrl: URL?
         let inviteTemplateId: String?
+        let name: String?
+        let untrustedMetadata: JSON?
         let locale: String?
+        let roles: [String]?
 
         /// - Parameters:
         ///   - email: The email of the member to send the invite magic link to.
         ///   - inviteRedirectUrl: The URL that the Member clicks from the invite Email Magic Link. This URL should be an endpoint in the backend server that verifies the request by querying Stytch's authenticate endpoint and finishes the invite flow. If this value is not passed, the default invite_redirect_url that you set in your Dashboard is used. If you have not set a default invite_redirect_url, an error is returned.
         ///   - inviteTemplateId: Use a custom template for invite emails. By default, it will use your default email template. The template must be a template using our built-in customizations or a custom HTML email for Magic Links - Invite.
+        ///   - name: The name of the Member.
+        ///   - untrustedMetadata: An arbitrary JSON object of application-specific data. These fields can be edited directly by the frontend SDK, and should not be used to store critical information.
         ///   - locale: Used to determine which language to use when sending the user this delivery method. Parameter is a IETF BCP 47 language tag, e.g. "en". Currently supported languages are English ("en"), Spanish ("es"), and Brazilian Portuguese ("pt-br"); if no value is provided, the copy defaults to English.
+        ///   - roles: Roles to explicitly assign to this Member.
         public init(
             email: String,
             inviteRedirectUrl: URL? = nil,
             inviteTemplateId: String? = nil,
-            locale: String? = nil
+            name: String? = nil,
+            untrustedMetadata: JSON? = nil,
+            locale: String? = nil,
+            roles: [String]? = nil
         ) {
             self.email = email
             self.inviteRedirectUrl = inviteRedirectUrl
             self.inviteTemplateId = inviteTemplateId
+            self.name = name
+            self.untrustedMetadata = untrustedMetadata
             self.locale = locale
+            self.roles = roles
         }
     }
 }
