85d05a642a65e98a343aabbba8bdaca6a4f4fcaf
Nidal Fakhouri <nfakhouri@stytch.com>
2024-07-10 14:44:55 -0400

    Conditionally use PKCE in magic links authenticate

--- Sources/StytchCore/StytchB2BClient/StytchB2BClient+MagicLinks.swift
+++ Sources/StytchCore/StytchB2BClient/StytchB2BClient+MagicLinks.swift
@@ -18,14 +18,23 @@ public extension StytchB2BClient {
         @Dependency(\.keychainClient) private var keychainClient
 
         // sourcery: AsyncVariants, (NOTE: - must use /// doc comment styling)
-        /// Wraps the magic link [authenticate](https://stytch.com/docs/b2b/api/authenticate-magic-link) API endpoint which validates the magic link token passed in. If this method succeeds, the member will be logged in, granted an active session, and the session cookies will be minted and stored in `HTTPCookieStorage.shared`.
+        /// Wraps the magic link [authenticate](https://stytch.com/docs/b2b/api/authenticate-magic-link) API endpoint which validates the magic link token passed in.
+        /// If this method succeeds, the member will be logged in, granted an active session, and the session cookies will be minted and stored in `HTTPCookieStorage.shared`.
         public func authenticate(parameters: AuthenticateParameters) async throws -> B2BAuthenticateResponse {
-            guard let codeVerifier: String = try? keychainClient.get(.codeVerifierPKCE) else { throw StytchSDKError.missingPKCE }
-
-            return try await router.post(
-                to: .authenticate,
-                parameters: CodeVerifierParameters(codingPrefix: .pkce, codeVerifier: codeVerifier, wrapped: parameters)
-            )
+            // For authenticating if loginOrSignup was called
+            if let codeVerifier: String = try? keychainClient.get(.codeVerifierPKCE) {
+                return try await router.post(
+                    to: .authenticate,
+                    parameters: CodeVerifierParameters(codingPrefix: .pkce, codeVerifier: codeVerifier, wrapped: parameters)
+                )
+            }
+            // For authenticating if inviteSend was called, in which case we will not have a PKCE challenge code
+            else {
+                return try await router.post(
+                    to: .authenticate,
+                    parameters: parameters
+                )
+            }
         }
 
         // sourcery: AsyncVariants, (NOTE: - must use /// doc comment styling)
