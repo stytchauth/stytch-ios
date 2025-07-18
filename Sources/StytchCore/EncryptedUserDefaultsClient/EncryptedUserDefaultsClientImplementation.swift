70ccce8c3c8b514c87ea7ec80aaaff259482f382
Jordan Haven <117691317+jhaven-stytch@users.noreply.github.com>
2025-07-18 12:27:21 -0400

    Add a DispatchQueue to EncryptedUserDefaults the same way we did to the KeychainClient; Don't trigger the startup flow (or attempt to retrieve the encryption key) until we are sure that protectedData is available (#506)

--- Sources/StytchCore/EncryptedUserDefaultsClient/EncryptedUserDefaultsClientImplementation.swift
+++ Sources/StytchCore/EncryptedUserDefaultsClient/EncryptedUserDefaultsClientImplementation.swift
@@ -1,63 +1,92 @@
 import CryptoKit
 import Foundation
 
-class EncryptedUserDefaultsClientImplementation: EncryptedUserDefaultsClient {
-    private let keychainClient: KeychainClient
+final class EncryptedUserDefaultsClientImplementation: EncryptedUserDefaultsClient {
+    static let shared = EncryptedUserDefaultsClientImplementation()
+    @Dependency(\.keychainClient) private var keychainClient
+    private let queue: DispatchQueue
+    private let queueKey = DispatchSpecificKey<Void>()
+
+    private var isOnQueue: Bool {
+        DispatchQueue.getSpecific(key: queueKey) != nil
+    }
 
     internal let defaults: UserDefaults = .init(suiteName: "StytchEncryptedUserDefaults") ?? .standard
 
-    init(keychainClient: KeychainClient) {
-        self.keychainClient = keychainClient
+    private init() {
+        queue = DispatchQueue(label: "StytchEncryptedUserDefaultsClientQueue")
+        queue.setSpecific(key: queueKey, value: ())
+    }
+
+    private func safelyEnqueue<T>(_ block: () throws -> T) throws -> T {
+        if isOnQueue {
+            return try block()
+        } else {
+            return try queue.sync { try block() }
+        }
     }
 
     func getItem(item: EncryptedUserDefaultsItem) throws -> EncryptedUserDefaultsItemResult? {
-        let userDefaultsData = defaults.data(forKey: item.name)
-        guard let decrypted = decryptData(encryptedData: userDefaultsData) else {
-            return nil
+        try safelyEnqueue {
+            let userDefaultsData = defaults.data(forKey: item.name)
+            guard let decrypted = decryptData(encryptedData: userDefaultsData) else {
+                return nil
+            }
+            guard let data = decrypted.data(using: .utf8) else {
+                return nil
+            }
+            return .init(data: data)
         }
-        guard let data = decrypted.data(using: .utf8) else {
-            return nil
-        }
-        return .init(data: data)
     }
 
     func itemExists(item: EncryptedUserDefaultsItem) -> Bool {
-        let encryptedData = defaults.data(forKey: item.name)
-        return encryptedData != nil
+        let result = try? safelyEnqueue {
+            let encryptedData = defaults.data(forKey: item.name)
+            return encryptedData != nil
+        }
+        return result ?? false
     }
 
     func setValueForItem(value: String?, item: EncryptedUserDefaultsItem) throws {
-        guard let valueString = value else {
-            return removeItem(item: item)
+        try safelyEnqueue {
+            guard let valueString = value else {
+                return removeItem(item: item)
+            }
+            let encryptedText = try encryptString(plainText: valueString)
+            defaults.set(encryptedText, forKey: item.name)
+            let encryptedDate = try encryptString(plainText: Date().asJson(encoder: Current.jsonEncoder))
+            defaults.set(encryptedDate, forKey: EncryptedUserDefaultsItem.lastValidatedAtDate(item.name).name)
         }
-        let encryptedText = try encryptString(plainText: valueString)
-        defaults.set(encryptedText, forKey: item.name)
-        let encryptedDate = try encryptString(plainText: Date().asJson(encoder: Current.jsonEncoder))
-        defaults.set(encryptedDate, forKey: EncryptedUserDefaultsItem.lastValidatedAtDate(item.name).name)
     }
 
     func removeItem(item: EncryptedUserDefaultsItem) {
-        defaults.removeObject(forKey: item.name)
-        defaults.removeObject(forKey: EncryptedUserDefaultsItem.lastValidatedAtDate(item.name).name)
+        try? safelyEnqueue {
+            defaults.removeObject(forKey: item.name)
+            defaults.removeObject(forKey: EncryptedUserDefaultsItem.lastValidatedAtDate(item.name).name)
+        }
     }
 
     private func encryptString(plainText: String) throws -> Data? {
-        guard let encryptionKey = keychainClient.encryptionKey else { return nil }
-        let sealedBox = try AES.GCM.seal(
-            Data(plainText.utf8),
-            using: encryptionKey
-        )
-        return sealedBox.combined
+        try safelyEnqueue {
+            guard let encryptionKey = keychainClient.encryptionKey else { return nil }
+            let sealedBox = try AES.GCM.seal(
+                Data(plainText.utf8),
+                using: encryptionKey
+            )
+            return sealedBox.combined
+        }
     }
 
     private func decryptData(encryptedData: Data?) -> String? {
-        guard let encryptionKey = keychainClient.encryptionKey, let encryptedData else { return nil }
-        do {
-            let sealedBox = try AES.GCM.SealedBox(combined: encryptedData)
-            let decryptedData = try AES.GCM.open(sealedBox, using: encryptionKey)
-            return String(data: decryptedData, encoding: .utf8)
-        } catch {
-            return nil
+        try? safelyEnqueue {
+            guard let encryptionKey = keychainClient.encryptionKey, let encryptedData else { return nil }
+            do {
+                let sealedBox = try AES.GCM.SealedBox(combined: encryptedData)
+                let decryptedData = try AES.GCM.open(sealedBox, using: encryptionKey)
+                return String(data: decryptedData, encoding: .utf8)
+            } catch {
+                return nil
+            }
         }
     }
 }
