be77ec9e21fe824862a9357950147ecee95d5390
Jordan Haven <jhaven@stytch.com>
2025-07-30 15:20:36 -0400

    Switch from timer to DispatchSourceTimer which ChatGPT assures me has no threading restrictions and will respect the custom queue being used; update tests as necessary

--- Tests/StytchCoreTests/BaseTestCase.swift
+++ Tests/StytchCoreTests/BaseTestCase.swift
@@ -21,7 +21,7 @@ class BaseTestCase: XCTestCase {
         Current.localStorage = .init()
         Current.timer = { _, _, _ in
             XCTFail("Unexpected timer initialization")
-            return .init()
+            return Self.mockTimer
         }
         Current.asyncAfter = { _, _, _ in
             XCTFail("Unexpected asyncAfter run")
@@ -200,3 +200,7 @@ class MockLocalAuthenticationContext: LAContextEvaluating {
     }
 }
 #endif
+
+extension BaseTestCase {
+    static var mockTimer = DispatchSource.makeTimerSource(queue: .main)
+}
