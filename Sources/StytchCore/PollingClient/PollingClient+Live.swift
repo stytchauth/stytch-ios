b80de449bdf6435d7913670a2e09f724b20e5192
Nidal Fakhouri <nfakhouri@stytch.com>
2025-09-24 11:56:39 -0400

    Configureable session extension

--- Sources/StytchCore/PollingClient/PollingClient+Live.swift
+++ Sources/StytchCore/PollingClient/PollingClient+Live.swift
@@ -3,7 +3,14 @@ import Foundation
 extension PollingClient {
     // A polling client which tries to complete every 3 minutes, and will retry w/ exponential backoff upon errors, for up to a total of approximately 124 seconds.
     static let sessions: PollingClient = .init(interval: 180, maxRetries: 5, queue: .sessionsPolling) { onSuccess, onFailure in
-        StytchClient.sessions.authenticate(parameters: .init()) { result in
+        let authenticateParameters: StytchClient.Sessions.AuthenticateParameters
+        if StytchClient.stytchClientConfiguration?.enableAutomaticSessionExtension == true {
+            authenticateParameters = .init(sessionDurationMinutes: StytchClient.defaultSessionDuration)
+        } else {
+            authenticateParameters = .init(sessionDurationMinutes: nil)
+        }
+
+        StytchClient.sessions.authenticate(parameters: authenticateParameters) { result in
             switch result {
             case .success:
                 onSuccess()
@@ -15,7 +22,14 @@ extension PollingClient {
 
     // A polling client which tries to complete every 3 minutes, and will retry w/ exponential backoff upon errors, for up to a total of approximately 124 seconds.
     static let memberSessions: PollingClient = .init(interval: 180, maxRetries: 5, queue: .sessionsPolling) { onSuccess, onFailure in
-        StytchB2BClient.sessions.authenticate(parameters: .init()) { result in
+        let authenticateParameters: StytchB2BClient.Sessions.AuthenticateParameters
+        if StytchB2BClient.stytchClientConfiguration?.enableAutomaticSessionExtension == true {
+            authenticateParameters = .init(sessionDurationMinutes: StytchB2BClient.defaultSessionDuration)
+        } else {
+            authenticateParameters = .init(sessionDurationMinutes: nil)
+        }
+
+        StytchB2BClient.sessions.authenticate(parameters: authenticateParameters) { result in
             switch result {
             case .success:
                 onSuccess()
